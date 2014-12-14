fbcf9cb420b9696345e40168f09521e0354b1029
Giovanni Blu Mitolo <gioscarab@gmail.com>
2014-12-14 08:18:35 +0100

    Encryption added, comments added

--- PJON.cpp
+++ PJON.cpp
@@ -2,40 +2,93 @@
  /*-O//\         __     __
    |-gfo\       |__| | |  | |\ |
    |!y°o:\      |  __| |__| | \|
-   |y"s§+`\     Giovanni Blu Mitolo 2011 - 2014
+   |y"s§+`\     Giovanni Blu Mitolo 2012 - 2014
   /so+:-..`\    gioscarab@gmail.com
   |+/:ngr-*.`\
-   |/:%&-a3f.:/\
-    \+//u/+gosv//\
-     \o+&/osw+odss\\
+   |/:%&-a3f.:/\     Simple library to send data from an Arduino board
+    \+//u/+gosv//\    to another one up to 4Kb/s with no trouble and
+     \o+&/osw+odss\\   on a single wire (plus GND obviously :P)
        \:/+-.§°-:+oss\
-       `|.|       \oy\\
-       ->-<
-       -||*/
+        | |       \oy\\
+        > <               Pull down resistor on the bus is generally used to reduce interference
+       -| |-
+
+ADDRESS: A byte is dedicated to the id of the board, so 255 different ids are selectable
+ACKNOLEDGE: Feedback to communication (receiver answer to sender ACK or NACK)
+COLLISION DETECTION: Transmissions collision avoidance
+CRC: XOR Cyclic Redundancy Check to ensure correct data communication
+ENCRYPTION: Custom private key encryption algorithm
+
+PJON Slow mode
+Absolute bandwidth 3.0 kb/s | Practical bandwidth 2.38 kb/s | Accuracy: 99.25%
+bit_width 20 - bit_spacer 68 - acceptance 16 - read_delay 9
+
+PJON Medium mode
+Absolute bandwidth 3.51 kb/b | Practical bandwidth 2.75 kb/s | Accuracy: 96.6%
+bit_width 18 - bit_spacer 36 - acceptance 16 - read_delay 7 */
 
 #include "WProgram.h"
 #include "WConstants.h"
 #include "includes/digitalWriteFast.h"
 #include "PJON.h"
 
+/* Initiate PJON passing pin number and the selected device id */
 PJON::PJON(int input_pin, byte device_id) {
   _input_pin = input_pin;
   _device_id = device_id;
 }
 
+/* Avoid to transmit over other transmitting devices:
+ Every device to speak together without transmissions collision.
+ On top of this can be developed a multimaster network of Arduino boards
+ that arbitrally decides to communicate (slight reduction of bandwidth) */
 void PJON::set_collision_avoidance(boolean state) {
   _collision_avoidance = state;
 }
 
+/* Sender waits for a feedback by receiver after transmission:
+ After the string is sent, receiver answer with an ACK if
+ CRC is ok or NAK if wrong (slight reduction of bandwidth) */
 void PJON::set_acknowledge(boolean state) {
   _acknowledge = state;
 }
 
+/* Encrypt transmitted data */
+void PJON::set_encryption(boolean state) {
+  _encryption = state;
+}
+
+/* Encrypt string with a custom made private key algorithm */
+void PJON::crypt(char *data) {
+ int i, j = 0;
+
+ for (i = 0; i < encryption_strength; i++)
+  _s_box[i] = i;
+
+ for (i = 0; i < encryption_strength; i++) {
+  j = (j + _s_box[i] + encryption_key[ i % strlen(encryption_key)]) % encryption_strength;
+  swap(_s_box[i], _s_box[j]);
+ }
+
+ i = j = 0;
+ for (int k = 0; k < strlen(data); k++) {
+  i = (i + 1) % encryption_strength;
+  j = (j + _s_box[i]) % encryption_strength;
+  swap(_s_box[i], _s_box[j]);
+  hash[k] = (data[k] ^ _s_box[ (_s_box[i] + _s_box[j]) % encryption_strength]) ^ encryption_key[3];
+  hash[k] = hash[k] ^ ((encryption_key[0] + i + encryption_key[1] + k + encryption_key[2]) % 255);
+ }
+
+ hash[strlen(data) + 1] = '\0';
+}
+
+/* Send a bit to the pin */
 void PJON::send_bit(byte VALUE, int duration) {
   digitalWriteFast(_input_pin, VALUE);
   delayMicroseconds(duration);
 }
 
+/* Send a byte to the pin */
 void PJON::send_byte(byte b) {
   pinModeFast(_input_pin, OUTPUT);
   this->send_bit(HIGH, bit_spacer);
@@ -45,22 +98,28 @@ void PJON::send_byte(byte b) {
     this->send_bit(b & mask, bit_width);
 }
 
-int PJON::send_string(byte ID, const char *string) {
+/* Send a string to the pin */
+int PJON::send_string(byte ID, char *string) {
+
   int package_length = strlen(string) + 3;
   byte CRC = 0;
 
   if(_collision_avoidance && !this->can_start())
       return BUSY;
 
+  if(_encryption)
+    this->crypt(string);
+
   this->send_byte(ID);
   CRC ^= ID;
   this->send_byte(package_length);
   CRC ^= package_length;
 
-  while(*string) {
-    this->send_byte(*string);
-    CRC ^= *string;
-    *string++;
+  char *string_pointer = (_encryption) ? hash : string;
+  while(*string_pointer) {
+    this->send_byte(*string_pointer);
+    CRC ^= *string_pointer;
+    *string_pointer++;
   }
 
   this->send_byte(CRC);
@@ -79,11 +138,13 @@ int PJON::send_string(byte ID, const char *string) {
 
 };
 
+/* Send a command to the pin */
 int PJON::send_command(byte ID, byte command_type, unsigned int value) {
   char bytes_to_send[3] = { command_type, value >> 8, value & 0xFF };
   return this->send_string(ID, bytes_to_send);
 }
 
+/* Receive a bit from the pin */
 int PJON::receive_bit() {
   delayMicroseconds((bit_width / 2) - read_delay);
   int bit_value = digitalReadFast(_input_pin);
@@ -91,9 +152,9 @@ int PJON::receive_bit() {
   return bit_value;
 }
 
+/* Receive a byte from the pin */
 byte PJON::receive_byte() {
   byte byte_value = B00000000;
-
   delayMicroseconds(bit_width / 2);
   for (int i = 0; i < 8; i++) {
     byte_value += digitalReadFast(_input_pin) << i;
@@ -103,16 +164,17 @@ byte PJON::receive_byte() {
   return byte_value;
 }
 
+/* Check if the channel if free for transmission */
 boolean PJON::can_start() {
   pinModeFast(_input_pin, INPUT);
   this->send_bit(0, 8);
-
   if(!this->receive_byte())
     return true;
 
   return false;
 }
 
+/* Check if a byte is coming from the pin */
 int PJON::start() {
   pinModeFast(_input_pin, INPUT);
   digitalWriteFast(_input_pin, LOW);
@@ -127,12 +189,12 @@ int PJON::start() {
   return FAIL;
 }
 
+/* Receive a string from the pin */
 int PJON::receive() {
   int package_length = max_package_length;
   byte CRC = 0;
 
   for (int i = 0; i < package_length; i++) {
-
     data[i] = this->start();
 
     if (data[i] == FAIL)
@@ -146,27 +208,27 @@ int PJON::receive() {
 
     if (i < package_length - 1)
       CRC ^= data[i];
-
   }
 
-  if(!_acknowledge)
-    if(data[package_length - 1] == CRC)
-      return ACK;
-    else
-      return NAK;
-
   if (data[package_length - 1] == CRC) {
+    if(_acknowledge) {
+      this->send_byte(ACK);
+      digitalWriteFast(_input_pin, LOW);
+    }
+    if(_encryption) {
+      for(int i = 0; i < package_length - 1; i++)
+        if(i > 1) data[i - 2] = data[i];
 
-    this->send_byte(ACK);
-    digitalWriteFast(_input_pin, LOW);
+      this->crypt((char*)data);
+    }
     return ACK;
 
   } else {
-
-    this->send_byte(NAK);
-    digitalWriteFast(_input_pin, LOW);
+    if(_acknowledge) {
+      this->send_byte(NAK);
+      digitalWriteFast(_input_pin, LOW);
+    }
     return NAK;
-
   }
 }
 
--- PJON.h
+++ PJON.h
@@ -2,16 +2,30 @@
  /*-O//\         __     __
    |-gfo\       |__| | |  | |\ |
    |!y°o:\      |  __| |__| | \|
-   |y"s§+`\     Giovanni Blu Mitolo 2011 - 2014
+   |y"s§+`\     Giovanni Blu Mitolo 2012 - 2014
   /so+:-..`\    gioscarab@gmail.com
   |+/:ngr-*.`\
-   |/:%&-a3f.:/\
-    \+//u/+gosv//\
-     \o+&/osw+odss\\
+   |/:%&-a3f.:/\     Simple library to send data from an Arduino board
+    \+//u/+gosv//\    to another one up to 4Kb/s with no trouble and
+     \o+&/osw+odss\\   on a single wire (plus GND obviously :P)
        \:/+-.§°-:+oss\
-       `|.|       \oy\\
-       ->-<
-       -||*/
+        | |       \oy\\
+        > <               Pull down resistor on the bus is generally used to reduce interference
+       -| |-
+
+ADDRESS: A byte is dedicated to the id of the board, so 255 different ids are selectable
+ACKNOLEDGE: Feedback on communication (receiver answer to sender ACK or NACK)
+COLLISION DETECTION: Transmissions collision avoidance
+CRC: XOR CRC ensure correct data communication
+ENCRYPTION: Custom private key encryption algorithm
+
+PJON Slow mode
+Absolute bandwidth 3.0 kb/s | Practical bandwidth 2.38 kb/s | Accuracy: 99.25%
+bit_width 20 - bit_spacer 68 - acceptance 16 - read_delay 9
+
+PJON Medium mode
+Absolute bandwidth 3.51 kb/b | Practical bandwidth 2.75 kb/s | Accuracy: 96.6%
+bit_width 18 - bit_spacer 36 - acceptance 16 - read_delay 7 */
 
 #ifndef PJON_h
 #define PJON_h
@@ -20,19 +34,11 @@
 #include "includes/digitalWriteFast.h"
 #endif
 
-// PJON Slow mode
-// Absolute bandwidth 3.0 kb/s | Practical bandwidth 2.38 kb/s | Accuracy: 99.25%
-// bit_width 20 - bit_spacer 68 - acceptance 16 - read_delay 9
-
-// PJON Medium mode
-// Absolute bandwidth 3.51 kb/b | Practical bandwidth 2.75 kb/s | Accuracy: 96.6%
-// bit_width 18 - bit_spacer 36 - acceptance 16 - read_delay 7
-
 // PJON Fast mode
 // Absolute bandwidth 3.40 kb/b | Practical bandwidth 2.7 kb/s | Accuracy: 89.9%
 // bit_width 17 - bit_spacer 36 - acceptance 17 - read_delay 5
 
-#define max_package_length 255
+#define max_package_length 256
 #define bit_width 18
 #define bit_spacer 36
 #define acceptance 16
@@ -43,6 +49,11 @@
 #define FAIL 0x100
 #define BUSY 666
 
+#define encryption_key "19id°?=(!$=<zkl"
+#define encryption_strength 2
+
+#define swap(a,b) do { int t = _s_box[a]; _s_box[a] = _s_box[b]; _s_box[b] = t; } while(0)
+
 class PJON {
 
   public:
@@ -50,25 +61,34 @@ class PJON {
     PJON(int input_pin, byte ID);
     void set_collision_avoidance(boolean state);
     void set_acknowledge(boolean state);
+    void set_encryption(boolean state);
+
+    void crypt(char *data);
 
     void send_bit(byte VALUE, int duration);
     void send_byte(byte b);
-    int send_string(byte ID, const char *string);
+
+    int send_string(byte ID, char *string);
     int send_command(byte ID, byte command_type, unsigned int value);
 
     int receive_bit();
     byte receive_byte();
+
     boolean can_start();
+
     int start();
     int receive();
 
     byte data[max_package_length];
+    char hash[max_package_length];
 
   private:
     byte _device_id;
     int _input_pin;
     int _read_delay;
+    unsigned char _s_box[16];
 
     boolean _acknowledge;
     boolean _collision_avoidance;
+    boolean _encryption;
 };

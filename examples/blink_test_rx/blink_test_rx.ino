a91a8b4f94be64d0682989c53611e5df880d8508
unknown <gioscarab@gmail.com>
2015-09-21 03:03:01 +0200

    Updated examples with new approaches

--- examples/blink_test_rx/blink_test_rx.ino
+++ examples/blink_test_rx/blink_test_rx.ino
@@ -1,25 +1,21 @@
 #include <PJON.h>
 
-float test;
-float mistakes;
-
 PJON network(A0, 44);
 
 void setup() {
-  Serial.begin(115200);
-  network.set_acknowledge(true);
-  network.set_collision_avoidance(true);
-  network.set_encryption(false);
-  network.insert_reaction('B', led_blink, false);
+  Serial.begin(9600);
+  network.set_receiver(receiver_function);
 };
 
-void led_blink() {
-  Serial.println("BLINK");
-  digitalWrite(13, HIGH);
-  delay(30);
-  digitalWrite(13, LOW);
+static void receiver_function(uint8_t length, uint8_t *payload) {
+  if(payload[0] == 'B') {
+    Serial.println("BLINK");
+    digitalWrite(13, HIGH);
+    delay(30);
+    digitalWrite(13, LOW);
+  }
 }
 
 void loop() {
-    network.receive(1000);
+  network.receive(1000);
 };

9ccea7300f2a40d258f3c7f209a522e67d0578c3
Giovanni Blu Mitolo <gioscarab@gmail.com>
2015-10-02 11:47:25 +0200

    Added BSD 3-Clause License

--- PJON.h
+++ PJON.h
@@ -25,7 +25,17 @@ ERROR HANDLING: Easy way to catch and program a reaction to network errors.
  |Transfer speed: 4.32kB/s     | Absolute bandwidth:  2.65 kB/s                     |
  |Baud rate:      32256 baud/s | Practical bandwidth: 2.05 kB/s                     |
  |Accuracy:       99.995%      |                                                    |
- |__________________________________________________________________________________| */
+ |__________________________________________________________________________________| 
+ 
+Copyright (c) 2013-2015, Giovanni Blu Mitolo All rights reserved.
+This software is provided by the copyright holders and contributors "as is" and any express or
+implied warranties, including, but not limited to, the implied warranties of merchantability and 
+fitness for a particular purpose are disclaimed. In no event shall the copyright holder or 
+contributors be liable for any direct, indirect, incidental, special, exemplary, or consequential
+damages (including, but not limited to, procurement of substitute goods or services; loss of use,
+data, or profits; or business interruption) however caused and on any theory of liability, whether 
+in contract, strict liability, or tort (including negligence or otherwise) arising in any way out 
+of the use of this software, even if advised of the possibility of such damage. */
 
 #ifndef PJON_h
   #define PJON_h

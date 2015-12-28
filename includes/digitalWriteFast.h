704554bb0981bb81526fa0186c98509a54cf7604
Giovanni Blu Mitolo <gioscarab@gmail.com>
2015-12-28 17:09:18 +0100

    Added dedicated digitalWriteFast library license

--- includes/digitalWriteFast.h
+++ includes/digitalWriteFast.h
@@ -1,4 +1,36 @@
 
+/* Updated version of digitalWriteFast library with ATmega 1280/2560 and ATtiny25/45/85 support
+   Copyright (c) 2012-2015, Giovanni Blu Mitolo All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+   
+    - Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+      
+    - Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+      
+    - All advertising materials mentioning features or use of this software
+      must display the following acknowledgement:
+      "This product includes digitalWriteFast software developed by Giovanni Blu Mitolo."
+      
+    - Neither the name of PJON, PJON_ASK, digitalWriteFast nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+   
+   This software is provided by the copyright holders and contributors"as is"
+   and any express or implied warranties, including, but not limited to, the
+   implied warranties of merchantability and fitness for a particular purpose
+   are disclaimed. In no event shall the copyright holder or contributors be
+   liable for any direct, indirect, incidental, special, exemplary, or consequential
+   damages (including, but not limited to, procurement of substitute goods or services;
+   loss of use, data, or profits; or business interruption) however caused and on any
+   theory of liability, whether in contract, strict liability, or tort (including
+   negligence or otherwise) arising in any way out of the use of this software, even if
+   advised of the possibility of such damage. */
+
 #if defined(ARDUINO) && (ARDUINO >= 100)
   #include "Arduino.h"
 #else

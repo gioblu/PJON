fe577922ed468307ccaafd4e9e0ef5e440dad995
unknown <gioscarab@gmail.com>
2015-12-28 04:07:10 +0100

    Removed mistake in receiver function documentation

--- PJON.cpp
+++ PJON.cpp
@@ -12,7 +12,7 @@
         | |       \oy\\  Pull down resistor on the bus is generally used to reduce interference.
         > <
        -| |-
-       
+
 Copyright (c) 2012-2015, Giovanni Blu Mitolo All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -24,7 +24,7 @@ modification, are permitted provided that the following conditions are met:
    notice, this list of conditions and the following disclaimer in the
    documentation and/or other materials provided with the distribution.
 
--  All advertising materials mentioning features or use of this software 
+-  All advertising materials mentioning features or use of this software
    must display the following acknowledgement:
    This product includes PJON software developed by Giovanni Blu Mitolo.
 
@@ -32,21 +32,21 @@ modification, are permitted provided that the following conditions are met:
    names of its contributors may be used to endorse or promote products
    derived from this software without specific prior written permission.
 
-This software is provided by the copyright holders and contributors"as is" 
-and any express or implied warranties, including, but not limited to, the 
-implied warranties of merchantability and fitness for a particular purpose 
-are disclaimed. In no event shall the copyright holder or contributors be 
-liable for any direct, indirect, incidental, special, exemplary, or consequential 
-damages (including, but not limited to, procurement of substitute goods or services; 
-loss of use, data, or profits; or business interruption) however caused and on any 
-theory of liability, whether in contract, strict liability, or tort (including 
-negligence or otherwise) arising in any way out of the use of this software, even if 
+This software is provided by the copyright holders and contributors"as is"
+and any express or implied warranties, including, but not limited to, the
+implied warranties of merchantability and fitness for a particular purpose
+are disclaimed. In no event shall the copyright holder or contributors be
+liable for any direct, indirect, incidental, special, exemplary, or consequential
+damages (including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused and on any
+theory of liability, whether in contract, strict liability, or tort (including
+negligence or otherwise) arising in any way out of the use of this software, even if
 advised of the possibility of such damage. */
 
 #include "PJON.h"
 
 /* Initiate PJON passing pin number:
-   Device's id has to be set through set_id() 
+   Device's id has to be set through set_id()
    before transmitting on the PJON network.  */
 
 PJON::PJON(int input_pin) {
@@ -84,15 +84,12 @@ void PJON::set_id(uint8_t id) {
 }
 
 
-/* Pass as a parameter a void function you previously defined in your code. 
-   This will be called when a correct message will be received. 
-   Inside there you can code how to react when data is received. 
-  
-   void receiver_function(uint8_t sender_id, uint8_t length, uint8_t *payload) {
-    Serial.print(sender_id);
-    Serial.print(" ");
+/* Pass as a parameter a void function you previously defined in your code.
+   This will be called when a correct message will be received.
+   Inside there you can code how to react when data is received.
 
-    for(int i = 0; i < length; i++) 
+   void receiver_function(uint8_t length, uint8_t *payload) {
+    for(int i = 0; i < length; i++)
       Serial.print((char)payload[i]);
       
     Serial.print(" ");
@@ -107,7 +104,7 @@ void PJON::set_receiver(receiver r) {
 
 
 /* Pass as a parameter a void function you previously defined in your code.
-   This will be called when an error in communication occurs 
+   This will be called when an error in communication occurs
 
 void error_handler(uint8_t code, uint8_t data) {
   Serial.print(code);
@@ -146,8 +143,8 @@ void PJON::send_bit(uint8_t VALUE, int duration) {
 }
 
 
-/* Every byte is prepended with 2 synchronization padding bits. The first 
-   is a longer than standard logic 1 followed by a standard logic 0. 
+/* Every byte is prepended with 2 synchronization padding bits. The first
+   is a longer than standard logic 1 followed by a standard logic 0.
    __________ ___________________________
   | SyncPad  | Byte                      |
   |______    |___       ___     _____    |
@@ -155,13 +152,13 @@ void PJON::send_bit(uint8_t VALUE, int duration) {
   | | 1  | 0 | 1 | 0 0 | 1 | 0 | 1 1 | 0 |
   |_|____|___|___|_____|___|___|_____|___|
     |
-   ACCEPTANCE 
+   ACCEPTANCE
 
-The reception tecnique is based on finding a logic 1 as long as the 
-first padding bit within a certain threshold, synchronizing to its 
-falling edge and checking if it is followed by a logic 0. If this 
-pattern is recognised, reception starts, if not, interference, 
-synchronization loss or simply absence of communication is 
+The reception tecnique is based on finding a logic 1 as long as the
+first padding bit within a certain threshold, synchronizing to its
+falling edge and checking if it is followed by a logic 0. If this
+pattern is recognised, reception starts, if not, interference,
+synchronization loss or simply absence of communication is
 detected at byte level. */
 
 void PJON::send_byte(uint8_t b) {
@@ -187,8 +184,8 @@ void PJON::send_byte(uint8_t b) {
 | 1 |0|0000|11|00| 1 |0|00000|1|00| 1 |0|0|1|000000| 1 |0|0|1|0000|1|0|
 |___|_|____|__|__|___|_|_____|_|__|___|_|_|_|______|___|_|_|_|____|_|_|
 
-A standard packet transmission is a bidirectional communication between 
-two devices that can be divided in 3 different phases: 
+A standard packet transmission is a bidirectional communication between
+two devices that can be divided in 3 different phases:
 
 Channel analysis   Transmission                            Response
     _____           _____________________________           _____
@@ -219,7 +216,7 @@ int PJON::send_string(uint8_t id, char *string, uint8_t length) {
   digitalWriteFast(_input_pin, LOW);
 
   if(id == BROADCAST) return ACK;
-  
+
   unsigned long time = micros();
   int response = FAIL;
 
@@ -227,32 +224,32 @@ int PJON::send_string(uint8_t id, char *string, uint8_t length) {
     response = this->receive_byte();
 
   if (response == ACK || response == NAK) return response;
-  
+
   return FAIL;
 };
 
 
 /* Insert a packet in the send list:
 
- The added packet will be sent in the next update() call. 
- Using the timing parameter you can set the delay between every 
+ The added packet will be sent in the next update() call.
+ Using the timing parameter you can set the delay between every
  transmission cyclically sending the packet (use remove() function stop it)
 
- int hi = network.send(99, "HI!", 3, 1000000); // Send hi every second  
+ int hi = network.send(99, "HI!", 3, 1000000); // Send hi every second
    _________________________________________________________________________
   |           |        |         |       |          |        |              |
   | device_id | length | content | state | attempts | timing | registration |
-  |___________|________|_________|_______|__________|________|______________| */ 
+  |___________|________|_________|_______|__________|________|______________| */
 
 int PJON::send(uint8_t id, char *packet, uint8_t length, unsigned long timing) {
 
   char *str = (char *) malloc(length);
-  
+
   if(str == NULL) {
     this->_error(MEMORY_FULL, FAIL);
     return FAIL;
   }
-     
+
   memcpy(str, packet, length);
 
   for(uint8_t i = 0; i < MAX_PACKETS; i++)
@@ -273,18 +270,18 @@ int PJON::send(uint8_t id, char *packet, uint8_t length, unsigned long timing) {
 }
 
 
-/* Update the state of the send list and so 
-   check if there are packets to be sent or to be erased 
-   the correctly delivered */ 
+/* Update the state of the send list and so
+   check if there are packets to be sent or to be erased
+   the correctly delivered */
 
 void PJON::update() {
   for(uint8_t i = 0; i < MAX_PACKETS; i++) {
     if(packets[i].state != NULL)
-      if(micros() - packets[i].registration > packets[i].timing + pow(packets[i].attempts, 2)) 
-        packets[i].state = send_string(packets[i].device_id, packets[i].content, packets[i].length); 
+      if(micros() - packets[i].registration > packets[i].timing + pow(packets[i].attempts, 2))
+        packets[i].state = send_string(packets[i].device_id, packets[i].content, packets[i].length);
 
     if(packets[i].state == ACK) {
-      if(!packets[i].timing)  
+      if(!packets[i].timing)
         this->remove(i);
       else {
         packets[i].attempts = 0;
@@ -292,9 +289,9 @@ void PJON::update() {
         packets[i].state = TO_BE_SENT;
       }
     }
-    if(packets[i].state == FAIL) { 
+    if(packets[i].state == FAIL) {
       packets[i].attempts++;
-  
+
       if(packets[i].attempts > MAX_ATTEMPTS) {
         this->_error(CONNECTION_LOST, packets[i].device_id);
         if(!packets[i].timing)
@@ -340,15 +337,15 @@ uint8_t PJON::syncronization_bit() {
 /* Check if a byte is coming from the pin:
 
  This function is looking for padding bits before a byte.
- If value is 1 for more then ACCEPTANCE and after 
+ If value is 1 for more then ACCEPTANCE and after
  that comes a 0 probably a byte is coming:
   ________
  |  Init  |
  |--------|
  |_____   |
  |  |  |  |
- |1 |  |0 |  
- |__|__|__| 
+ |1 |  |0 |
+ |__|__|__|
     |
   ACCEPTANCE */
 
@@ -432,7 +429,7 @@ int PJON::receive(unsigned long duration) {
     for(int i = 0; i < 3706; i++) {
       response = this->receive();
       if(response == ACK) {
-        this->_receiver(data[1] - 3, data + 2); 
+        this->_receiver(data[1] - 3, data + 2);
         return ACK;
       }
     }

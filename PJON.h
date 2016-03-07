d2212bbe3375d03113c9d906c57e3b991591566b
240974a <240974a@gmail.com>
2016-03-07 13:15:38 +0300

    support ESP 8266 (80mHz in COMPATIBILITY_MODE)

--- PJON.h
+++ PJON.h
@@ -52,7 +52,13 @@ advised of the possibility of such damage. */
   #define PJON_h
 
   #include "Arduino.h"
-  #include "includes/digitalWriteFast.h"
+#ifdef __AVR__
+#  include "includes/digitalWriteFast.h"
+#else
+#  define digitalWriteFast digitalWrite
+#  define digitalReadFast digitalRead
+#  define pinModeFast pinMode
+#endif
 
   /* COMPATIBILITY_MODE set to true:
   Run the network with a cautious speed to support all architectures.
@@ -113,6 +119,14 @@ advised of the possibility of such damage. */
       #define READ_DELAY 15
     #endif
   #endif
+  #if defined(ESP8266) 
+   #if (F_CPU == 80000000L || COMPATIBILITY_MODE) 
+	  #define BIT_WIDTH  44
+	  #define BIT_SPACER 110
+	  #define ACCEPTANCE 35
+	  #define READ_DELAY 4
+   #endif  
+  #endif  
 
 #endif
 

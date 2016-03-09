7e893b9a39ed9b73113919d7ae2540f11f0909d6
EsbenSoeltoft <relaxcold@gmail.com>
2016-03-09 20:47:07 +0100

    Added files via upload

--- PJON.h
+++ PJON.h
@@ -48,6 +48,42 @@ theory of liability, whether in contract, strict liability, or tort (including
 negligence or otherwise) arising in any way out of the use of this software, even if
 advised of the possibility of such damage. */
 
+/* modified 2016-03-09 by Esben Soeltoft
+
+   - Added support for the Arduino Zero. 
+     Able to achieve the following speeds between two Arduino Zero
+       Absolute com speed: 5215.20B/s
+       Practical bandwidth: 4346.00B/s
+       Packets sent: 2173.00
+       Mistakes (error found with CRC) 0.00
+       Fail (no answer from receiver) 0
+       Busy (Channel is busy or affected by interference) 0
+       Accuracy: 100.00 %  
+   
+   - All comparison typecasted to preserve proper format between platforms. Comparisons
+     on AVR defaults to 16 bit unless typecasted, while it defaults to 32 bits on SAMD
+     This can give problems when comparison is made with timer functions such as millis(),
+     micros(), delay() and delaymicroseconds().
+     The function micros() and millis() both returns a unsigned long (uint32_t) on AVR and SAMD.
+     Delay argument is typecasted (unsigned long) or (uint32_t) on AVR, and delaymicroseconds argument 
+     is typecasted (unsigned int) or (uint16_t) on AVR, while on SAMD the delay argument is typecasted 
+     (uint32_t), and delaymicroseconds argument is typecasted (uint32_t).
+
+   - #endif belonging to #ifndef PJON_h moved to end of file
+
+   - in function receive_byte, the functioncall digitalWriteFast(_input_pin,LOW) has been removed. It
+     should not be necessary when the _input_pin is pulled low using a pulldown resistor. Having this
+     second line only slows down.
+     If the physical pulldown resistor has to be avoided, pinModeFast(_input_pin,INPUT_PULLDOWN) will
+     be the correct function call to use, as the INPUT_PULLDOWN is done through pinMode and not
+     digitalWriteFast.
+     
+   TO-DO
+   - Reduce variable size to optimize memory footprint
+
+*/
+
+
 #ifndef PJON_h
   #define PJON_h
 
@@ -62,59 +98,68 @@ advised of the possibility of such damage. */
   indicated only for networks made by a group of devices with the
   same architecture / processor (for example 10 Arduino Uno) */
 
-  #define  COMPATIBILITY_MODE true
+  #define  COMPATIBILITY_MODE false
 
   /* The following constants setup are quite conservative and determined only
      with a huge amount of time and blind testing (without oscilloscope)
      tweaking values and analysing results. Theese can be changed to obtain
      faster speed. Probably you need experience, time and an oscilloscope. */
 
-  #if defined(__AVR_ATmega88__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__)
-    #if (F_CPU == 16000000 && !COMPATIBILITY_MODE)
-      #define BIT_WIDTH 20
-      #define BIT_SPACER 56
-      #define ACCEPTANCE 20
-      #define READ_DELAY 8
-    #endif
-    #if (F_CPU == 8000000 || COMPATIBILITY_MODE)
-      #define BIT_WIDTH 40
-      #define BIT_SPACER 112
-      #define ACCEPTANCE 40
-      #define READ_DELAY 4
-    #endif
-  #endif
 
-  #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-    #if (F_CPU == 16000000 && !COMPATIBILITY_MODE)
-      /* Mega has shorter values than Duemianove / Uno because
-      micros() produces longer delays on ATmega1280/2560 */
+  #if (COMPATIBILITY_MODE)
+    #define BIT_WIDTH 40
+    #define BIT_SPACER 112
+    #define ACCEPTANCE 40
+    #define READ_DELAY 4
+  #else
+    /* Values known to work
+       BIT_WIDTH 18
+       BIT_SPACER 54
+       ACCEPTANCE 18
+       READ_DELAY 8
+    */
+    #if defined(ARDUINO_SAMD_ZERO ) 
+      #define BIT_WIDTH 14
+      #define BIT_SPACER 42
+      #define ACCEPTANCE 14
+      #define READ_DELAY 4
+/*    #elif ( (F_CPU == 48000000)|| defined(ARDUINO_ARCH_SAMD) || defined(__SAMD21G18A__) )
       #define BIT_WIDTH 18
       #define BIT_SPACER 54
       #define ACCEPTANCE 18
-      #define READ_DELAY 8
-    #endif
-    #if (F_CPU == 8000000 || COMPATIBILITY_MODE)
-      /* Mega has shorter values than Duemianove / Uno because
-      micros() produces longer delays on ATmega1280/2560 */
-      #define BIT_WIDTH 38
-      #define BIT_SPACER 110
-      #define ACCEPTANCE 38
-      #define READ_DELAY 16
-    #endif
-  #endif
-
-  #if defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-    #if (F_CPU == 8000000 || COMPATIBILITY_MODE) // Internal clock
+      #define READ_DELAY 8 */
+    #elif defined(__AVR_ATmega88__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__)
+      #if ( F_CPU == 16000000 )
+        /* Mega has shorter values than Duemianove / Uno because
+        micros() produces longer delays on ATmega1280/2560 */      
+        #define BIT_WIDTH 18
+        #define BIT_SPACER 54
+        #define ACCEPTANCE 18
+        #define READ_DELAY 8
+      #else  // internal clock 8000000
+        #define BIT_WIDTH 38
+        #define BIT_SPACER 110
+        #define ACCEPTANCE 38
+        #define READ_DELAY 16
+      #endif
+    #elif defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+      // Internal clock F_CPU == 80000
       /* ATtiny has shorter values than Duemianove / Uno because
       micros() produces longer delays on ATtiny45/85 */
       #define BIT_WIDTH  35
       #define BIT_SPACER 107
       #define ACCEPTANCE 35
       #define READ_DELAY 15
-    #endif
+    #else
+      #define BIT_WIDTH 40
+      #define BIT_SPACER 112
+      #define ACCEPTANCE 40
+      #define READ_DELAY 4    
+    #endif            
   #endif
 
-#endif
+
+ 
 
 // Protocol symbols
 #define ACK        6
@@ -144,9 +189,9 @@ struct packet {
   uint8_t device_id;
   char *content;
   uint8_t length;
-  unsigned long registration;
-  int state;
-  unsigned long timing;
+  uint32_t registration;
+  uint16_t state;
+  uint32_t timing;
 };
 
 typedef void (* receiver)(uint8_t length, uint8_t *payload);
@@ -158,8 +203,8 @@ static void dummy_receiver_handler(uint8_t length, uint8_t *payload) {};
 class PJON {
 
   public:
-    PJON(int input_pin);
-    PJON(int input_pin, uint8_t id);
+    PJON(uint8_t input_pin);
+    PJON(uint8_t input_pin, uint8_t id);
 
     void initialize();
 
@@ -168,17 +213,17 @@ class PJON {
     void set_receiver(receiver r);
     void set_error(error e);
 
-    int receive_byte();
-    int receive();
-    int receive(unsigned long duration);
+    uint16_t receive_byte();
+    uint16_t receive();
+    uint16_t receive(uint32_t duration);
 
-    void send_bit(uint8_t VALUE, int duration);
+    void send_bit(uint8_t VALUE, uint32_t duration);
     void send_byte(uint8_t b);
-    int  send_string(uint8_t id, char *string, uint8_t length);
-    int  send(uint8_t id, char *packet, uint8_t length, unsigned long timing = 0);
+    uint16_t  send_string(uint8_t id, char *string, uint8_t length);
+    uint16_t  send(uint8_t id, char *packet, uint8_t length, uint32_t timing = 0);
 
     void update();
-    void remove(int id);
+    void remove(uint16_t id);
 
     uint8_t read_byte();
     boolean can_start();
@@ -190,7 +235,8 @@ class PJON {
 
   private:
     uint8_t   _device_id;
-    int       _input_pin;
+    uint8_t   _input_pin;
     receiver  _receiver;
     error     _error;
 };
+#endif

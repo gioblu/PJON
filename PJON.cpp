324f02c27f48aac652a4f264fff234493e776a6f
Giovanni Blu Mitolo <gioscarab@gmail.com>
2016-04-13 21:07:52 +0200

    Update PJON.cpp

--- PJON.cpp
+++ PJON.cpp
@@ -13,32 +13,19 @@
         > <
   _____-| |-________________________________________________________________________
 
-  Released with BSD-3 License.
-  PJON Copyright (c) 2012-2016, Giovanni Blu Mitolo All rights reserved.
+Copyright 2012-2016 Giovanni Blu Mitolo
 
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-  - Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-  -  Redistributions in binary form must reproduce the above copyright
-     notice, this list of conditions and the following disclaimer in the
-     documentation and/or other materials provided with the distribution.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-  -  Neither the name of PJON, PJON_ASK nor the
-     names of its contributors may be used to endorse or promote products
-     derived from this software without specific prior written permission.
-
-  This software is provided by the copyright holders and contributors"as is"
-  and any express or implied warranties, including, but not limited to, the
-  implied warranties of merchantability and fitness for a particular purpose
-  are disclaimed. In no event shall the copyright holder or contributors be
-  liable for any direct, indirect, incidental, special, exemplary, or consequential
-  damages (including, but not limited to, procurement of substitute goods or services;
-  loss of use, data, or profits; or business interruption) however caused and on any
-  theory of liability, whether in contract, strict liability, or tort (including
-  negligence or otherwise) arising in any way out of the use of this software, even if
-  advised of the possibility of such damage. */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 #include "PJON.h"
 

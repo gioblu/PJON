2a7a3571de0ac7009069ad258d14f9685ce3c2a7
unknown <gioscarab@gmail.com>
2016-05-01 20:23:56 +0200

    Added SendAndReceive example

---
+++ examples/SendAndReceive/Device2/Device2.ino
@@ -0,0 +1,28 @@
+#include <PJON.h>
+
+// bus(Arduino pin used, selected device id)
+PJON bus(12, 45);
+
+void setup() {
+  pinModeFast(13, OUTPUT);
+  digitalWriteFast(13, LOW); // Initialize LED 13 to be off
+  bus.begin(); // Initialize PJON bus
+
+  bus.set_receiver(receiver_function);
+  bus.send(44, "B", 1);
+  Serial.begin(115200);
+};
+
+void receiver_function(uint8_t length, uint8_t *payload) {
+ if((char)payload[0] == 'B') {
+    bus.send(44, "B", 1);
+    digitalWrite(13, HIGH);
+    delay(15);
+    digitalWrite(13, LOW);
+  }
+}
+
+void loop() {
+  bus.receive(1000);
+  bus.update();
+};

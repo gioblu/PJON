f46680bf656f3bb27b4f9002009e6009918cc86c
unknown <gioscarab@gmail.com>
2016-09-03 17:45:32 +0200

    SoftwareBitBang update to contain set_pin and set_pins

--- strategies/SoftwareBitBang/SoftwareBitBang.h
+++ strategies/SoftwareBitBang/SoftwareBitBang.h
@@ -1,7 +1,6 @@
 
-/* SoftwareBitBang One wire interrupts-less implementation
-   Part of the PJON framework (included in version v3.0)
-   Copyright (c) 2012-2016, Giovanni Blu Mitolo All rights reserved.
+/* SoftwareBitBang 1 or 2 wires interrupts-less digital communication Transport layer
+   Used as a Strategy by the PJON framework (included in version v3.0)
 
 Performance
 
@@ -56,7 +55,21 @@ your loop cycle to avoid huge blind timeframes.
 are using interrupts in their procedure, like for example the Servo library.
 Reliability or bandwidth loss can be experienced because of the cyclical
 interruptions made by third party interrupt driven software to the PJON code.
+____________________________________________________________________________
 
+Copyright 2012-2016 Giovanni Blu Mitolo gioscarab@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 STANDARD transmission mode performance:
    Transfer speed: 16.944kBb or 2.12kB/s
@@ -78,6 +91,7 @@ STANDARD transmission mode performance:
 #define _SWBB_MODE _SWBB_STANDARD
 
 #include "Timing.h"
+#include "../../utils/digitalWriteFast.h"
 
 class SoftwareBitBang {
   public:
@@ -86,17 +100,17 @@ class SoftwareBitBang {
     If receiving 10 bits no 1s are detected
     there is no active transmission */
 
-    boolean can_start(uint8_t input_pin, uint8_t output_pin) {
-      pinModeFast(input_pin, INPUT);
+    boolean can_start() {
+      pinModeFast(_input_pin, INPUT);
       for(uint8_t i = 0; i < 9; i++) {
-        if(digitalReadFast(input_pin))
+        if(digitalReadFast(_input_pin))
           return false;
         delayMicroseconds(SWBB_BIT_WIDTH);
       }
-      if(digitalReadFast(input_pin)) return false;
-      pinModeFast(output_pin, OUTPUT);
+      if(digitalReadFast(_input_pin)) return false;
+      pinModeFast(_output_pin, OUTPUT);
       return true;
-    }
+    };
 
 
     /* Every byte is prepended with 2 synchronization padding bits. The first
@@ -117,16 +131,24 @@ class SoftwareBitBang {
     synchronization loss or simply absence of communication is
     detected at byte level. */
 
-    void send_byte(uint8_t b, uint8_t input_pin, uint8_t output_pin) {
-      digitalWriteFast(output_pin, HIGH);
+    void send_byte(uint8_t b) {
+      digitalWriteFast(_output_pin, HIGH);
       delayMicroseconds(SWBB_BIT_SPACER);
-      digitalWriteFast(output_pin, LOW);
+      digitalWriteFast(_output_pin, LOW);
       delayMicroseconds(SWBB_BIT_WIDTH);
       for(uint8_t mask = 0x01; mask; mask <<= 1) {
-        digitalWriteFast(output_pin, b & mask);
+        digitalWriteFast(_output_pin, b & mask);
         delayMicroseconds(SWBB_BIT_WIDTH);
       }
-    }
+    };
+
+
+    /* Send a string: */
+
+    void send_string(uint8_t *string, uint8_t length) {
+      for(uint8_t b = 0; b < length; b++)
+        send_byte(string[b]);
+    };
 
 
     /* Syncronize with transmitter:
@@ -136,32 +158,32 @@ class SoftwareBitBang {
      in which portion of the bit, the reading will be
      executed by the next read_byte function */
 
-    uint8_t syncronization_bit(uint8_t pin) {
+    uint8_t syncronization_bit() {
       delayMicroseconds((SWBB_BIT_WIDTH / 2) - SWBB_READ_DELAY);
-      uint8_t bit_value = digitalReadFast(pin);
+      uint8_t bit_value = digitalReadFast(_input_pin);
       delayMicroseconds(SWBB_BIT_WIDTH / 2);
       return bit_value;
-    }
+    };
 
 
     /* Read a byte from the pin */
 
-    uint8_t read_byte(uint8_t pin) {
+    uint8_t read_byte() {
       uint8_t byte_value = B00000000;
       /* Delay until the center of the first bit */
       delayMicroseconds(SWBB_BIT_WIDTH / 2);
       for(uint8_t i = 0; i < 7; i++) {
         /* Read in the center of the n one */
-        byte_value += digitalReadFast(pin) << i;
+        byte_value += digitalReadFast(_input_pin) << i;
         /* Delay until the center of the next one */
         delayMicroseconds(SWBB_BIT_WIDTH);
       }
       /* Read in the center of the last one */
-      byte_value += digitalReadFast(pin) << 7;
+      byte_value += digitalReadFast(_input_pin) << 7;
       /* Delay until the end of the bit */
       delayMicroseconds(SWBB_BIT_WIDTH / 2);
       return byte_value;
-    }
+    };
 
 
     /* Check if a byte is coming from the pin:
@@ -178,47 +200,66 @@ class SoftwareBitBang {
         |
       ACCEPTANCE */
 
-    uint16_t receive_byte(uint8_t input_pin, uint8_t output_pin) {
+    uint16_t receive_byte() {
       /* Initialize the pin and set it to LOW to reduce interference */
-      pullDownFast(input_pin);
+      pullDownFast(_input_pin);
 
-      if(output_pin != input_pin && output_pin != NOT_ASSIGNED)
-        pullDownFast(output_pin);
+      if(_output_pin != _input_pin && _output_pin != NOT_ASSIGNED)
+        pullDownFast(_output_pin);
 
       uint32_t time = micros();
       /* Do nothing until the pin goes LOW or passed more time than SWBB_BIT_SPACER duration */
-      while(digitalReadFast(input_pin) && (uint32_t)(micros() - time) <= SWBB_BIT_SPACER);
+      while(digitalReadFast(_input_pin) && (uint32_t)(micros() - time) <= SWBB_BIT_SPACER);
       /* Save how much time passed */
       time = micros() - time;
       /* is for sure equal or less than SWBB_BIT_SPACER, and if is more than ACCEPTANCE
          (a minimum HIGH duration) and what is coming after is a LOW bit
          probably a byte is coming so try to receive it. */
-      if(time >= SWBB_ACCEPTANCE && !syncronization_bit(input_pin))
-        return (uint8_t)read_byte(input_pin);
+      if(time >= SWBB_ACCEPTANCE && !syncronization_bit())
+        return (uint8_t)read_byte();
       return FAIL;
-    }
+    };
 
 
     /* Receive byte response */
 
-    uint16_t receive_response(uint8_t input_pin, uint8_t output_pin) {
-
-      if(output_pin != input_pin && output_pin != NOT_ASSIGNED)
-        digitalWriteFast(output_pin, LOW);
+    uint16_t receive_response() {
+      if(_output_pin != _input_pin && _output_pin != NOT_ASSIGNED)
+        digitalWriteFast(_output_pin, LOW);
 
       uint16_t response = FAIL;
       uint32_t time = micros();
       while(response == FAIL && (uint32_t)((micros() - SWBB_BIT_SPACER) - SWBB_BIT_WIDTH) <= time)
-        response = receive_byte(input_pin, output_pin);
+        response = receive_byte();
       return response;
-    }
+    };
 
 
     /* Send byte response to package transmitter */
 
-    void send_response(uint8_t response, uint8_t input_pin, uint8_t output_pin) {
-      pinModeFast(output_pin, OUTPUT);
-      send_byte(response, input_pin, output_pin);
-      pullDownFast(output_pin);
-    }
+    void send_response(uint8_t response) {
+      pinModeFast(_output_pin, OUTPUT);
+      send_byte(response);
+      pullDownFast(_output_pin);
+    };
+
+
+    /* Set the communicaton pin: */
+
+    void set_pin(uint8_t pin) {
+      _input_pin = pin;
+      _output_pin = pin;
+    };
+
+
+    /* Set a pair of communication pins: */
+
+    void set_pins(uint8_t input_pin = NOT_ASSIGNED, uint8_t output_pin = NOT_ASSIGNED) {
+      _input_pin = input_pin;
+      _output_pin = output_pin;
+    };
+
+  private:
+    uint8_t _input_pin;
+    uint8_t _output_pin;
 };

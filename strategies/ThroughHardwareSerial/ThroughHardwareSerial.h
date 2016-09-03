0fbeed5954c84713646716820679098ea231a635
unknown <gioscarab@gmail.com>
2016-09-03 17:46:14 +0200

    ThroughHardwareSerial update to new Strategy format

--- strategies/ThroughHardwareSerial/ThroughHardwareSerial.h
+++ strategies/ThroughHardwareSerial/ThroughHardwareSerial.h
@@ -1,7 +1,6 @@
 
-/* ThroughHardwareSerial enables PJON communication through the Serial port.
-   Copyright (c) 2016 by Giovanni Blu Mitolo and Fred Larsen All rights reserved.
-
+/* ThroughHardwareSerial digital communication transport layer
+   used as a Strategy by the PJON framework (included in version v4.1)
    With ThroughHardwareSerial PJON can run through the Serial port of your device.
    Both ports should be free from usb computer connection and data pin should
    be wired inverted (TX->RX, RX->TX).
@@ -23,7 +22,23 @@
      - Connect 2 slaves with a 1 master having univocal communication with device id
 
    ThroughHardwareSerial can run a 2 device bus in multi-master or a n devices bus
-   in a master-slave setup. (n still unkown, tested with 1 master and 2 slaves) */
+   in a master-slave setup. (n still unkown, tested with 1 master and 2 slaves)
+   ____________________________________________________________________________
+
+   ThroughHardwareSerial, Copyright (c) 2016 by Giovanni Blu Mitolo
+   and Fred Larsen All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
 
 #include <Arduino.h>
 
@@ -42,22 +57,30 @@ class ThroughHardwareSerial {
 
     /* Returns the Serial object value i.e. if(Serial) */
 
-    boolean can_start(uint8_t input_pin, uint8_t output_pin) {
+    boolean can_start() {
       return (serial != NULL) && *serial; // Check if pointer
     };
 
 
     /* Send a byte and wait for its transmission end */
 
-    void send_byte(uint8_t b, uint8_t input_pin, uint8_t output_pin) {
+    void send_byte(uint8_t b) {
       serial->write(b);
       serial->flush();
     };
 
 
+    /* Send a string: */
+
+    void send_string(uint8_t *string, uint8_t length) {
+      for(uint8_t b = 0; b < length; b++)
+        send_byte(string[b]);
+    };
+
+
     /* Try to receive a byte with a maximum waiting time */
 
-    uint16_t receive_byte(uint8_t input_pin, uint8_t output_pin) {
+    uint16_t receive_byte() {
       uint32_t time = micros();
       while(micros() - time < THROUGH_HARDWARE_SERIAL_MAX_TIME)
         if(serial->available() > 0)
@@ -68,14 +91,14 @@ class ThroughHardwareSerial {
 
     /* Receive byte response */
 
-    uint16_t receive_response(uint8_t input_pin, uint8_t output_pin) {
-      return receive_byte(input_pin, output_pin);
+    uint16_t receive_response() {
+      return receive_byte();
     };
 
 
     /* Send byte response to the packet's transmitter */
 
-    void send_response(uint8_t response, uint8_t input_pin, uint8_t output_pin) {
-      send_byte(response, input_pin, output_pin);
+    void send_response(uint8_t response) {
+      send_byte(response);
     };
 };

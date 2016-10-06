074612f931eada6fa0c987424555f388ea9692a6
unknown <gioscarab@gmail.com>
2016-10-06 15:14:58 +0200

    avoid useless inclusion

--- strategies/ThroughSerial/ThroughSerial.h
+++ strategies/ThroughSerial/ThroughSerial.h
@@ -24,7 +24,6 @@
    limitations under the License. */
 
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
 #define THROUGH_SERIAL_MAX_BYTE_TIME        10000  // Wait up to 10 milliseconds for an incoming byte
 #define THROUGH_SERIAL_FREE_TIME_BEFORE_START 500  // 0.5 milliseconds of free channell before sending

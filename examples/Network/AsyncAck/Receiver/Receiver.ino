5d11cc6b844c82dbd96d0e8dfd35986eaffaba30
Radiusxe <radius.soukroma@gmail.com>
2016-12-31 03:11:28 +0100

    Update Receiver.ino
    replaced digitalWrite to digitalWriteFast

--- examples/Network/AsyncAck/Receiver/Receiver.ino
+++ examples/Network/AsyncAck/Receiver/Receiver.ino
@@ -20,9 +20,9 @@ void setup() {
 
 void receiver_function(uint8_t *payload, uint16_t length, const PacketInfo &packet_info) {
   if(payload[0] == 'B') {
-    digitalWrite(13, HIGH);
+    digitalWriteFast(13, HIGH);
     delay(3);
-    digitalWrite(13, LOW);
+    digitalWriteFast(13, LOW);
     delay(3);
   }
 }

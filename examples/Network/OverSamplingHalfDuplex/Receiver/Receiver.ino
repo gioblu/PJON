31d631397faa333c77a95cdb6c2a39b6573eecf9
unknown <gioscarab@gmail.com>
2017-01-08 19:59:40 +0100

    remove useless print

--- examples/Network/OverSamplingHalfDuplex/Receiver/Receiver.ino
+++ examples/Network/OverSamplingHalfDuplex/Receiver/Receiver.ino
@@ -19,26 +19,6 @@ void setup() {
 };
 
 void receiver_function(uint8_t *payload, uint16_t length, const PacketInfo &packet_info) {
-  Serial.print("Header: ");
-  Serial.print(packet_info.header, BIN);
-  // If packet formatted for a shared medium
-  Serial.print(" Receiver bus id: ");
-  Serial.print(packet_info.receiver_bus_id[0]);
-  Serial.print(packet_info.receiver_bus_id[1]);
-  Serial.print(packet_info.receiver_bus_id[2]);
-  Serial.print(packet_info.receiver_bus_id[3]);
-  Serial.print(" Device id: ");
-  Serial.print(packet_info.receiver_id);
-  // If sender info is included
-  if((packet_info.header & SENDER_INFO_BIT) != 0) {
-    Serial.print(" Sender bus id: ");
-    Serial.print(packet_info.sender_bus_id[0]);
-    Serial.print(packet_info.sender_bus_id[1]);
-    Serial.print(packet_info.sender_bus_id[2]);
-    Serial.print(packet_info.sender_bus_id[3]);
-    Serial.print(" device id: ");
-    Serial.println(packet_info.sender_id);
-  }
   if(payload[0] == 'B') {
     digitalWriteFast(13, HIGH);
     delay(5);

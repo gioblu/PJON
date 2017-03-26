9e45c5c9570b7e61291e9affaa877ad286075454
unknown <gioscarab@gmail.com>
2017-03-26 05:55:02 +0200

    receive_string method added to AS and OS

--- strategies/AnalogSampling/AnalogSampling.h
+++ strategies/AnalogSampling/AnalogSampling.h
@@ -208,6 +208,19 @@ class AnalogSampling {
     };
 
 
+    /* Receive a string: */
+
+    uint16_t receive_string(uint8_t *string, uint16_t max_length) {
+      uint16_t result;
+      for(uint16_t b = 0; b < max_length; b++) {
+        result = receive_byte();
+        if(result == PJON_FAIL) return b;
+        string[b] = result;
+      }
+      return max_length;
+    };
+
+
     /* Every byte is prepended with 2 synchronization padding bits. The first
        is a longer than standard logic 1 followed by a standard logic 0.
        __________ ___________________________

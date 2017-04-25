edeeac78cbfbc49f80f654fe0e53832c8fd3f748
unknown <gioscarab@gmail.com>
2017-04-25 04:39:03 +0200

    update link change

--- interfaces/PJON_Interfaces.h
+++ interfaces/PJON_Interfaces.h
@@ -1,6 +1,6 @@
 
 #pragma once
 
-#include "Arduino/PJON_Arduino_Interface.h"
+#include "ARDUINO/PJON_ARDUINO_Interface.h"
 #include "RPI/PJON_RPI_Interface.h"
 #include "WINX86/PJON_WINX86_Interface.h"

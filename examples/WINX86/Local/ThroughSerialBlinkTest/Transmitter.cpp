76eb4f31135a8224812ee5fa5db66a1ac92cfba5
unknown <gioscarab@gmail.com>
2017-05-03 06:41:42 +0200

    WINX86 clearer example

--- examples/WINX86/Local/ThroughSerialBlinkTest/Transmitter.cpp
+++ examples/WINX86/Local/ThroughSerialBlinkTest/Transmitter.cpp
@@ -2,22 +2,22 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <iostream>
-
 // PJON library
 #include <inttypes.h>
 #include <stdlib.h>
 #include <string.h>
 #include <chrono>
 
-#include "PJON/interfaces/WINX86/Serial/Serial.h"
-
 #ifndef WINX86
   #define WINX86 true
 #endif
 
-#define PJON_INCLUDE_TS true // Include only ThroughSerial
-#include "PJON/PJON.h"
-#include "PJON/PJONDefines.h"
+#define TS_RESPONSE_TIME_OUT 25000
+/* WINX86 machine generally needs longer response timeout to correctly
+   receive a response */
+
+#define PJON_INCLUDE_TS true
+#include "PJON\PJON.h"
 
 static void receiver_function(
   uint8_t *payload,
@@ -25,17 +25,17 @@ static void receiver_function(
   const PJON_Packet_Info &packet_info
 ) {
   for (uint32_t i = 0; i != length; i++)
-    std::cout <<  payload[i];
+    std::cout << payload[i];
   std::cout << std::endl;
 };
 
 int main() {
   printf("PJON instantiation... \n");
   PJON<ThroughSerial> bus(45);
+  bus.set_receiver(receiver_function);
 
-  bool resetComOnStratup = false;
-  bool testComOnStartup = false;
-  tstring commPortName(TEXT("COM3"));
+  // Set here the COM port assigned to the device you want to communicate with
+  tstring commPortName(TEXT("COM9"));
   int bitRate = 9600;
 
   try {
@@ -43,42 +43,23 @@ int main() {
     Serial serial_handle(
       commPortName,
       bitRate,
-      testComOnStartup,
-      resetComOnStratup
+      false,
+      false
     );
-
-    if(resetComOnStratup)
-      delayMicroseconds(2000000);
 
     printf("Setting serial... \n");
     bus.strategy.set_serial(&serial_handle);
-
     printf("Opening bus... \n");
     bus.begin();
-    bus.set_receiver(receiver_function);
+    printf("Success, initiating BlinkTest repeated transmission... \n");
 
-    for(int i = 0; i < 500; i++) {
-      bus.send(44, "BCD123456789BCD123456789", 24);
-
-      auto begin_ts = std::chrono::high_resolution_clock::now();
-      int bus_update_ms = 15;
-      while(true) {
-        auto elapsed_usec =
-          std::chrono::duration_cast<std::chrono::microseconds>(
-            std::chrono::high_resolution_clock::now() - begin_ts
-          ).count();
-
-        if(elapsed_usec >= bus_update_ms * 1000) break;
-        bus.update();
-        bus.receive(100000);
-      }
-    }
-
-    printf("Attempting to receive from bus... \n");
-    bus.receive(100);
-    printf("Success! \n");
-    return 0;
-  } catch (const char* msg) {
+    while(true)
+      if(bus.send_packet(44, "B", 1) == PJON_ACK) {
+        bus.receive(1000000);
+        printf("Device blinked as requested! \n");
+      } else printf("Failure! \n");
+  }
+  catch (const char* msg) {
     printf("exc: ");
     printf(msg);
     printf("\n");

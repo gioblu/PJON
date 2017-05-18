4719ee7ed002046c7046891ef843827f6de3f24f
unknown <gioscarab@gmail.com>
2017-05-19 01:51:50 +0200

    ThroughSerial correct call order

--- examples/ARDUINO/Local/ThroughSerialBlinkTest/Receiver/Receiver.ino
+++ examples/ARDUINO/Local/ThroughSerialBlinkTest/Receiver/Receiver.ino
@@ -7,11 +7,11 @@ void setup() {
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW); // Initialize LED 13 to be off
 
-  bus.begin();
-  bus.set_receiver(receiver_function);
-
   Serial.begin(9600);
   bus.strategy.set_serial(&Serial);
+  bus.begin();
+
+  bus.set_receiver(receiver_function);
 };
 
 void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
--- examples/ARDUINO/Local/ThroughSerialBlinkTest/Transmitter/Transmitter.ino
+++ examples/ARDUINO/Local/ThroughSerialBlinkTest/Transmitter/Transmitter.ino
@@ -4,11 +4,11 @@
 PJON<ThroughSerial> bus(45);
 
 void setup() {
-  bus.begin();
   Serial.begin(9600);
   bus.strategy.set_serial(&Serial); // Pass the Serial object you want to use for communication
+  bus.begin();
   bus.send_repeatedly(44, "B", 1, 1000000); // Send B to device 44 every second
-}
+};
 
 void loop() {
   bus.update();

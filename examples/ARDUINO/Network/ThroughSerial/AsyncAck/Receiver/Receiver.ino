7a615509f3fb0a46fdc569d9fb9893c9d36bd87f
unknown <gioscarab@gmail.com>
2017-06-28 16:15:28 +0200

    examples strategy dir added, new network SWBB ThroughSerial async example

---
+++ examples/ARDUINO/Network/ThroughSerial/AsyncAck/Receiver/Receiver.ino
@@ -0,0 +1,35 @@
+
+/* Include Async ACK code setting PJON_INCLUDE_ASYNC_ACK as true before including PJON.h */
+#define PJON_INCLUDE_ASYNC_ACK true
+#include <PJON.h>
+
+uint8_t bus_id[] = {0, 0, 0, 1};
+// <Strategy name> bus(selected device id)
+PJON<ThroughSerial> bus(bus_id, 44);
+
+void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW); // Initialize LED 13 to be off
+
+  Serial.begin(9600);
+  bus.strategy.set_serial(&Serial);
+  bus.set_synchronous_acknowledge(false);
+  bus.begin();
+  bus.set_receiver(receiver_function);
+};
+
+void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
+  /* Make use of the payload before sending something, the buffer where payload points to is
+     overwritten when a new message is dispatched */
+  if(payload[0] == 'B') {
+    digitalWrite(13, HIGH);
+    delay(3);
+    digitalWrite(13, LOW);
+    delay(3);
+  }
+}
+
+void loop() {
+  bus.update();
+  bus.receive(1000);
+};

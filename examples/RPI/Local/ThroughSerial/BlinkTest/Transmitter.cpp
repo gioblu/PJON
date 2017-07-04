e8ab099cec1ea130b79cc71715f25b2ee1613455
unknown <gioscarab@gmail.com>
2017-07-04 20:30:30 +0200

    RPI example bugfix, added missing TIMEOUT config

--- examples/RPI/Local/ThroughSerial/BlinkTest/Transmitter.cpp
+++ examples/RPI/Local/ThroughSerial/BlinkTest/Transmitter.cpp
@@ -13,6 +13,12 @@
   #define RPI true
 #endif
 
+#define TS_RESPONSE_TIME_OUT 35000
+/* Maximum accepted timeframe between transmission and synchronous
+   acknowledgement. This timeframe is affected by latency and CRC computation.
+   Could be necessary to higher this value if devices are separated by long
+   physical distance and or if transmitting long packets. */
+   
 #define PJON_INCLUDE_TS true // Include only ThroughSerial
 #include "PJON/PJON.h"
 

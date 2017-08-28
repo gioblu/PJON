c7020c59dc8f13e4712f811f8dc494718c1286e8
unknown <gioscarab@gmail.com>
2017-08-28 11:32:18 +0200

    GlobalUDP examples

---
+++ examples/ARDUINO/Local/GlobalUDP/PingPong/Transmitter/Transmitter.ino
@@ -0,0 +1,42 @@
+#include <PJON.h>
+
+// Ethernet configuration for this device
+byte gateway[] = { 192, 1, 1, 1 };
+byte subnet[] = { 255, 255, 255, 0 };
+byte mac[] = {0xDA, 0xCA, 0x7E, 0xEF, 0xFE, 0x5D};
+uint8_t local_ip[] = { 192, 1, 1, 150 };
+
+// Address of remote device
+uint8_t remote_ip[] = { 192, 1, 1, 151 };
+
+// <Strategy name> bus(selected device id)
+PJON<GlobalUDP> bus(45);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Transmitter started.");
+  Ethernet.begin(mac, local_ip, gateway, gateway, subnet);
+
+  bus.strategy.add_node(44, remote_ip);
+  bus.set_receiver(receiver_function);
+  bus.begin();
+  bus.send_repeatedly(44, "P", 1, 100000); // Send P to device 44 repeatedly
+}
+
+uint32_t cnt = 0;
+uint32_t start = millis();
+
+void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
+  if (payload[0] == 'P') cnt++;
+}
+
+void loop() {
+  bus.update();
+  bus.receive();
+
+  if (millis() - start > 1000) {
+    start = millis();
+    Serial.print("PONG/s: "); Serial.println(cnt);
+    cnt = 0;
+  }
+};

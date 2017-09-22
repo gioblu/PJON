e0fb4ff6578b23a935d98aa6d18a20bfd2d4513a
unknown <gioscarab@gmail.com>
2017-09-23 00:40:05 +0200

    PacketSeparationTest removed consequent false positives count

--- examples/ARDUINO/Local/SoftwareBitBang/PacketSeparationTest/Receiver/Receiver.ino
+++ examples/ARDUINO/Local/SoftwareBitBang/PacketSeparationTest/Receiver/Receiver.ino
@@ -17,13 +17,9 @@ SoftwareBitBang swbb;
 uint32_t attempts =  100000; // Number of random string reception attempts
 uint8_t data[1];
 
-uint32_t test = 0;
-uint32_t fail = 0;
-uint32_t time = 0;
-
-uint32_t last_false_positive = 0;
-uint32_t max_false_positive_length = 0;
-
+uint32_t test;
+uint32_t fail;
+uint32_t time;
 uint8_t percent;
 
 void setup() {
@@ -37,10 +33,8 @@ void print_result() {
   Serial.print("Attempts: ");
   Serial.print(test);
   Serial.print(" - False positives: ");
-  Serial.println(fail);
-  Serial.print("Maximum false positive length: ");
-  Serial.print(max_false_positive_length + (fail) ? 1 : 0);
-  Serial.print("B - Accuracy: ");
+  Serial.print(fail);
+  Serial.print(" - Accuracy: ");
   Serial.print(100.0 - (100.0 / (float(test) / float(fail))), 5);
   Serial.println("%");
   Serial.print("Elapsed time: ");
@@ -52,8 +46,7 @@ void print_result() {
 };
 
 void loop() {
-  fail = 0, test = 0, percent = 0,
-  max_false_positive_length = 0, last_false_positive = 0;
+  fail = 0, test = 0, percent = 0;
   Serial.println();
   Serial.print("Executing SoftwareBitBang");
   Serial.println(" packet separation consistency test.");
@@ -65,12 +58,8 @@ void loop() {
   test = 0, fail = 0, percent = 0;
   time = millis();
   while(test < attempts) {
-    if(swbb.receive_string(data, PJON_PACKET_MAX_LENGTH) != PJON_FAIL) {
-      if(test && (last_false_positive == (test - 1)))
-        max_false_positive_length++;
-      last_false_positive = test;
+    if(swbb.receive_string(data, PJON_PACKET_MAX_LENGTH) != PJON_FAIL)
       fail++;
-    }
     test++;
     if(!(test % (attempts / 50))) {
       Serial.print("-");

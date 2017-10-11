89c3c1f7055b9d28dba02a24c1f7e00530ff2a91
unknown <gioscarab@gmail.com>
2017-10-11 07:34:07 +0200

    TS RS485 sync ack blink example

---
+++ examples/ARDUINO/Local/ThroughSerial/RS485-Blink/Transmitter/Transmitter.ino
@@ -0,0 +1,34 @@
+
+/* PJON MAX485 serial modules RS485 communication example */
+
+#include <PJON.h>
+
+uint8_t bus_id[] = {0, 0, 0, 1};
+// <Strategy name> bus(selected device id)
+PJON<ThroughSerial> bus(bus_id, 45);
+
+void setup() {
+  // Initialize LED 13 to be off
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  // Initialize Serial instance used for PJON communication
+  Serial.begin(9600);
+  bus.strategy.set_serial(&Serial);
+  // Set enable pins
+  bus.strategy.set_RS485_rxe_pin(3);
+  bus.strategy.set_RS485_txe_pin(2);
+  bus.set_error(error_handler);
+  bus.begin();
+  // Send B to device 44 every 1.5s
+  bus.send_repeatedly(44, "B", 1, 1500000);
+}
+
+void error_handler(uint8_t code, uint8_t data) {
+  if(code == PJON_CONNECTION_LOST)
+    digitalWrite(13, HIGH); // Light up LED 13 if a packet transmission failed
+}
+
+void loop() {
+    bus.update();
+    bus.receive();
+};

fb00c9007b746d6ae088019e4c20cf538cf41690
unknown <gioscarab@gmail.com>
2017-10-11 14:31:12 +0200

    TS 20 max attempts, degree 4

--- strategies/ThroughSerial/Timing.h
+++ strategies/ThroughSerial/Timing.h
@@ -57,10 +57,10 @@
 
 /* Maximum transmission attempts */
 #ifndef TS_MAX_ATTEMPTS
-  #define TS_MAX_ATTEMPTS         5
+  #define TS_MAX_ATTEMPTS         20
 #endif
 
 /* Back-off exponential degree */
 #ifndef TS_BACK_OFF_DEGREE
-  #define TS_BACK_OFF_DEGREE      5
+  #define TS_BACK_OFF_DEGREE      4
 #endif

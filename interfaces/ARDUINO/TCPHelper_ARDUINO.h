d1a21f401e418e1d718a08b1ad90668db1c426ec
unknown <gioscarab@gmail.com>
2017-10-17 12:34:14 +0200

    avoid duplicated ESP8266 related ifdefs

--- interfaces/ARDUINO/TCPHelper_ARDUINO.h
+++ interfaces/ARDUINO/TCPHelper_ARDUINO.h
@@ -1,39 +1,30 @@
 #pragma once
 
-#ifdef ARDUINO_ESP8266_WEMOS_D1MINI // WeMos mini and D1 R2
-  #define PJON_ESP
-#elif ARDUINO_ESP8266_ESP01     // Generic ESP's use for 01's
-  #define PJON_ESP
-#elif ARDUINO_ESP8266_NODEMCU   // Wio Link and NodeMCU 1.0 (also 0.9), use for ESP12
-  #define PJON_ESP
-#endif
-
 #ifdef PJON_ESP
-#include <ESP8266WiFi.h>
-typedef WiFiServer TCPHelperServer;
-typedef WiFiClient TCPHelperClient;
-#define min(a,b) (a<b?a:b)
+  #include <ESP8266WiFi.h>
+  typedef WiFiServer TCPHelperServer;
+  typedef WiFiClient TCPHelperClient;
+  #define min(a,b) (a<b?a:b)
 #else
-#include <EthernetClient.h>
-#include <utility/socket.h>
-typedef EthernetServer TCPHelperServer;
-typedef EthernetClient TCPHelperClient;
+  #include <EthernetClient.h>
+  #include <utility/socket.h>
+  typedef EthernetServer TCPHelperServer;
+  typedef EthernetClient TCPHelperClient;
 #endif
-
 
 /*
-// The following classes make it posssible to use the same non-blocking connect logic 
-// on Arduino as on POSIX. It works, allowing better communication with POSIX devices, 
+// The following classes make it posssible to use the same non-blocking connect logic
+// on Arduino as on POSIX. It works, allowing better communication with POSIX devices,
 // but requires that the official EthernetClient and EthernetServer classes are changed
 // so that private members are made protected, making it possible to add
 // functionality to them.
 
 class TCPHelperClient : public EthernetClient {
-public:  
+public:
   bool prepare_connect(IPAddress ip, uint16_t port) {
-    if (_sock != MAX_SOCK_NUM) 
+    if (_sock != MAX_SOCK_NUM)
       stop();
-    
+
     if (_sock != MAX_SOCK_NUM)
       return false;
 
@@ -59,7 +50,7 @@ public:
 
     return true;
   }
- 
+
   int try_connect() {
     if (status() == SnSR::ESTABLISHED) return 1;
     if (status() == SnSR::CLOSED) {
@@ -77,7 +68,7 @@ public:
 };
 
 class TCPHelperServer : public EthernetServer {
-public:  
+public:
   TCPHelperServer(uint16_t sock) : EthernetServer(sock) { }
 
   TCPHelperClient available() {

62451c3c8452e72b548105bda120b8960e43bb48
unknown <gioscarab@gmail.com>
2017-10-28 20:32:58 +0200

    SWBB examples mode comments update

--- examples/ARDUINO/Local/SoftwareBitBang/PacketSeparationTest/Transmitter/Transmitter.ino
+++ examples/ARDUINO/Local/SoftwareBitBang/PacketSeparationTest/Transmitter/Transmitter.ino
@@ -6,9 +6,9 @@
    The receiver device attempts to receive packets from pin 12.
    If an incoming packet is detected a false positive occurred.  */
 
-// Uncomment to run SoftwareBitBang to mode FAST
+// Uncomment to run SoftwareBitBang in MODE 2
 // #define SWBB_MODE 2
-// Uncomment to run SoftwareBitBang to mode OVERDRIVE
+// Uncomment to run SoftwareBitBang in MODE 3
 // #define SWBB_MODE 3
 
 #include <PJON.h>

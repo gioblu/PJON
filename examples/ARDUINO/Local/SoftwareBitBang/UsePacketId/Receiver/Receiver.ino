b721c7486bc8eaf83f4a34d6255cc50817de7674
unknown <gioscarab@gmail.com>
2017-12-22 13:42:14 +0100

    Packet id usage example added

---
+++ examples/ARDUINO/Local/SoftwareBitBang/UsePacketId/Receiver/Receiver.ino
@@ -0,0 +1,79 @@
+// Include packet id feature
+#define PJON_INCLUDE_PACKET_ID true
+
+// Uncomment to run SoftwareBitBang in MODE 2
+// #define SWBB_MODE 2
+// Uncomment to run SoftwareBitBang in MODE 3
+// #define SWBB_MODE 3
+
+#include <PJON.h>
+
+float test;
+float mistakes;
+int busy;
+int fail;
+
+// <Strategy name> bus(selected device id)
+PJON<SoftwareBitBang> bus(44);
+
+void setup() {
+  bus.strategy.set_pin(12);
+  bus.set_receiver(receiver_function);
+  bus.begin();
+
+  Serial.begin(115200);
+};
+
+void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
+  Serial.print("Packet id: ");
+  Serial.println(packet_info.id);
+  Serial.flush();
+}
+
+void loop() {
+  Serial.println("Starting 1 second communication speed test...");
+  long time = millis();
+  unsigned int response = 0;
+  while(millis() - time < 1000) {
+    response = bus.receive();
+    if(response == PJON_ACK)
+      test++;
+    if(response == PJON_NAK)
+      mistakes++;
+    if(response == PJON_BUSY)
+      busy++;
+    if(response == PJON_FAIL)
+      fail++;
+  }
+
+  Serial.print("Packet Overhead: ");
+  Serial.print(bus.packet_overhead(bus.last_packet_info.header) + 1);
+  Serial.print("B - Total: ");
+  Serial.print((unsigned int)((bus.packet_overhead(bus.last_packet_info.header) + 1) * test));
+  Serial.println("B");
+  Serial.print("Absolute com speed: ");
+  Serial.print(test * (20 + bus.packet_overhead(bus.last_packet_info.header) + 1));
+  Serial.println("B/s");
+  Serial.print("Practical bandwidth: ");
+  Serial.print(test * 20);
+  Serial.println("B/s");
+  Serial.print("Packets sent: ");
+  Serial.println(test);
+  Serial.print("Mistakes (error found with CRC) ");
+  Serial.println(mistakes);
+  Serial.print("Fail (no answer from receiver) ");
+  Serial.println(fail);
+  Serial.print("Busy (Channel is busy or affected by interference) ");
+  Serial.println(busy);
+  Serial.print("Accuracy: ");
+  Serial.print(100 - (100 / (test / mistakes)));
+  Serial.println(" %");
+  Serial.println(" --------------------- ");
+  // Avoid Serial interference during test flushing
+  Serial.flush();
+
+  test = 0;
+  mistakes = 0;
+  busy = 0;
+  fail = 0;
+};

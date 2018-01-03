d0866825010307c6206e3fdd38e3194fae990128
Matheus Eduardo Garbelini <mgarbelix@gmail.com>
2018-01-03 12:04:33 -0200

    Initial LoRa support
    To enable LoRa strategy "#define PJON_INCLUDE_TL" must be added in the
    sketch

---
+++ examples/ARDUINO/Local/LoRa/BlinkTest/Transmitter/Transmitter.ino
@@ -0,0 +1,19 @@
+#define PJON_INCLUDE_TL
+#include <PJON.h>
+/*
+To use this example, please download the LoRa third party Library from https://github.com/sandeepmistry/arduino-LoRa/
+*/
+// <Strategy name> bus(selected device id)
+PJON<ThroughLora> bus(45);
+
+void setup() {
+	Serial.begin(9600);
+	bus.strategy.setFrequency(868100000UL); //Obrigatory to initialize Radio with correct frequency
+	bus.strategy.setSignalBandwidth(250E3); //Optional
+	bus.begin();
+	bus.send_repeatedly(44, "B", 1, 1000000); // Send B to device 44 every second
+};
+
+void loop() {
+	bus.update();
+};

09bd0b485269564ef641fb7d748e2f51315c4298
unknown <gioscarab@gmail.com>
2018-02-10 02:01:05 +0100

    Added comments/license to Any, StrategyLink and StrategyLinkBase

--- strategies/Any/Any.h
+++ strategies/Any/Any.h
@@ -1,3 +1,26 @@
+
+/* Any strategy
+   This strategy includes virtual inheritance and let a PJON object switch
+   from a strategy to another when required or a collection of PJON objects
+   with different strategies to be treated dynamically.
+
+   Proposed and developed by Fred Larsen
+   ___________________________________________________________________________
+
+    Copyright 2010-2018 Giovanni Blu Mitolo gioscarab@gmail.com
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License. */
+
 #pragma once
 
 #include <strategies/Any/StrategyLinkBase.h>

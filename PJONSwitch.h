46f9e16a1afe85d68d89ab8ef5ad74f976183f35
fredilarsen <fredilarsen@hotmail.com>
2018-02-23 16:52:56 +0100

    Cleanup of PJONSwitch.h

--- PJONSwitch.h
+++ PJONSwitch.h
@@ -13,20 +13,10 @@
         > <
  ______-| |-__________________________________________________________________
 
-PJONSimpleSwitch has been contributed by Fred Larsen.
+PJONSwitch has been contributed by Fred Larsen.
 
-It routes packets between buses with different bus ids, and between segmented
-buses as well, even for local buses with no bus ids.
-
-A default gateway can be specified, identifying one of the attached buses to
-receive packets to other target buses than any of the attached buses. It is
-possible to use a PJONSimpleSwitch to handle leaf buses in a tree structure.
-
-A segmented bus is a "virtual" bus where ranges of its devices are  located 
-in separate physical buses. This allows a single local "virtual" or segmented
-bus to consist of buses of different strategies. This can be used to work in
-local mode, without bus ids, even if spanning different media, to reduce the
-packet sizes. A switch can combine multiple media into one bus.
+This class does the same as the PJONSimpleSwitch but supports routing packets
+between buses of different strategies using the Any strategy.
 
 If you believe in this project and you appreciate our work, please, make a
 donation. The PJON Foundation is entirely financed by contributions of wise
@@ -81,26 +71,7 @@ public:
 };
 
 class PJONSwitch : public PJONSimpleSwitch<Any> {
-protected:
-/*
-  virtual uint8_t find_bus_with_id(
-    const uint8_t bus_id[],
-    const uint8_t device_id,
-    uint8_t &start_bus
-  ) {
-    return find_attached_bus_with_id(bus_id, device_id, start_bus);
-  };
-
-  virtual void dynamic_receiver_function(
-    uint8_t *payload,
-    uint16_t length,
-    const PJON_Packet_Info &packet_info
-  ) {
-    PJONSimpleSwitch<Any>::dynamic_receiver_function(payload, length, packet_info);
-  };
-*/
 public:
-
   PJONSwitch() : PJONSimpleSwitch<Any>() {};
 
   PJONSwitch(

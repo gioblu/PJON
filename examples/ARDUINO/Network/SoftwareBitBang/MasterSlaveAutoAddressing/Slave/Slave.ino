693d22857485910f226987d9cae2b5d17e129fc2
unknown <gioscarab@gmail.com>
2018-03-09 15:42:48 +0100

    PJONSlave example update

--- examples/ARDUINO/Network/SoftwareBitBang/MasterSlaveAutoAddressing/Slave/Slave.ino
+++ examples/ARDUINO/Network/SoftwareBitBang/MasterSlaveAutoAddressing/Slave/Slave.ino
@@ -10,27 +10,44 @@ int packet;
 char content[] = "01234567890123456789";
 uint32_t time;
 
+void receiver_handler(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
+  Serial.print("Received: ");
+  for(uint16_t i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+};
+
+void error_handler(uint8_t code, uint8_t data, void *custom_pointer) {
+  if(code == PJON_CONNECTION_LOST) {
+    Serial.print("Connection lost with device ");
+    Serial.println((uint8_t)bus.packets[data].content[0], DEC);
+  }
+  if(code == PJON_ID_ACQUISITION_FAIL) {
+    if(data == PJON_ID_ACQUIRE)
+      Serial.println("Multi-master auto addressing procedure failed.");
+    if(data == PJON_ID_CONFIRM)
+      Serial.println("Master-slave id confirmation procedure failed.");
+    if(data == PJON_ID_NEGATE)
+      Serial.println("Master-slave id release procedure failed.");
+    if(data == PJON_ID_REQUEST)
+      Serial.println("Master-slave id request procedure failed.");
+  }
+};
+
 void setup() {
   bus.set_error(error_handler);
+  bus.set_receiver(receiver_handler);
   bus.strategy.set_pin(12);
   bus.begin();
   Serial.begin(115200);
   time = millis();
-}
-
-void error_handler(uint8_t code, uint8_t data, void *custom_pointer) {
-  if(code == PJON_CONNECTION_LOST) {
-    Serial.print("Connection lost with master ");
-    Serial.println(bus.packets[data].content[0], DEC);
-  }
-  if(code == PJON_ID_ACQUISITION_FAIL) {
-    Serial.print("Unable to acquire an id: ");
-    Serial.println(data);
-  }
+  bus.acquire_id_master_slave();
 }
 
 void loop() {
-  if(millis() - time > 3000) {
+  if((millis() - time) > 3000) {
     Serial.println(bus.device_id());
     time = millis();
   }

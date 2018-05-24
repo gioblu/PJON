5a8f4dd6d6190bc3f640ded9f8f49409b2c36add
Juergen Skrotzky <JorgenVikingGod@gmail.com>
2018-05-24 15:15:55 +0200

    add ESP32 support (#207)

--- src/interfaces/ARDUINO/TCPHelper_ARDUINO.h
+++ src/interfaces/ARDUINO/TCPHelper_ARDUINO.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #ifdef PJON_ESP
-  #include <ESP8266WiFi.h>
+  #if defined(ESP32)
+    #include <WiFi.h>
+  #else
+    #include <ESP8266WiFi.h>
+  #endif
   typedef WiFiServer TCPHelperServer;
   typedef WiFiClient TCPHelperClient;
   #define min(a,b) (a<b?a:b)

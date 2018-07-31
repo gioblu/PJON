93995402e40d3e0a2fa95988ace8f788a61f6b25
sticilface <amelvin@gmail.com>
2018-07-31 21:59:33 +0100

    remove debug code add readme

--- src/strategies/ThroughAysncSerial/README.md
+++ src/strategies/ThroughAysncSerial/README.md
@@ -1,45 +1,47 @@
-### ThroughSerial
+### ThroughAsyncSerial
 
 **Medium:** Hardware/Software Serial port |
 **Pins used:** 1 or 2
 
-With `ThroughSerial` strategy, PJON can run through a software or hardware Serial port working out of the box with many Arduino compatible serial transceivers, like RS485 or radio modules like HC-12 (HCMODU0054). Take a look at the [video introduction](https://www.youtube.com/watch?v=H4jUsgvM-lw) for a brief showcase of its features. It complies with [TSDL v2.0](/src/strategies/ThroughSerial/specification/TSDL-specification-v2.0.md).
+With `ThroughAsyncSerial` strategy, PJON can run through a software or hardware Serial port working out of the box with many Arduino compatible serial transceivers, like RS485 or radio modules like HC-12 (HCMODU0054). Take a look at the [video introduction](https://www.youtube.com/watch?v=H4jUsgvM-lw) for a brief showcase of its features. It complies with [TSDL v2.0](/src/strategies/ThroughSerial/specification/TSDL-specification-v2.0.md).  This strategy is based upon the ThroughSerial Strategy but uses the hardware serial buffers present on most Arduino devices (incuding ESP8266).  It is not required to call bus.receive() with any delay, just call is frequently to see if there are any packets that have been received.  Your loop can now run 10000's of times per second and speeds of 200 packets a second are possible between ESP32 and Leonado at 115200 baud. 
 
 #### Why PJON over Serial?
-Serial communication is fast and reliable but it is often useless without all the features PJON contains. `ThroughSerial` has been developed to enable PJON communication through a serial data link. Adding PJON on top of Serial it is possible to leverage of the PJON protocol layer features like acknowledge, addressing, multiplexing, packet handling, 8 or 32-bit CRC and traffic control.
+Serial communication is fast and reliable but it is often useless without all the features PJON contains. `ThroughAsyncSerial` has been developed to enable PJON communication through a serial data link. Adding PJON on top of Serial it is possible to leverage of the PJON protocol layer features like acknowledge, addressing, multiplexing, packet handling, 8 or 32-bit CRC and traffic control.  This version is very suited to ESO8266 and ESP32 where polling using SWBB is not ideal.  Receive is entirely non-blocking.  Sending, however, and ACK are still blocking. There is a default timeout on checking data that is 100us.  This is to allow data to accumulate in the harware UART buffer.  This value is changable with `bus.strategy.set_timeout(0)` and may require adjustment depending on UART RX buffer size and baud rate.  
 
-#### How to use ThroughSerial
+#### How to use ThroughAsyncSerial
 Pass the `ThroughSerial` type as PJON template parameter to instantiate a PJON object ready to communicate through this Strategy.
 ```cpp  
-PJON<ThroughSerial> bus;
+#include PJON_INCLUDE_TAS
+PJON<ThroughAsyncSerial> bus;
 ```
 Call the `begin` method on the `Serial` or `SoftwareSerial`  object you want to use for PJON communication and pass it to the `set_serial` method:
 ```cpp  
 /* Set 5 milliseconds (default) as the maximum timeframe for
    byte reception. Always set:
    TX_MAX_RESPONSE_TIME > (byte transmission time + latency) */
-#define TS_BYTE_TIME_OUT      5000
+#define TAS_BYTE_TIME_OUT      5000
 
 /* Set 10 milliseconds as the maximum timeframe between
    transmission and synchronous acknowledgement response.
    This value is strictly related to the maximum time needed
    by receiver to receive and compute the packet.
    Higher if necessary. */
-#define TS_RESPONSE_TIME_OUT 10000
+#define TAS_RESPONSE_TIME_OUT 10000
 
 /* Set the back-off exponential degree (default 4) */
-#define TS_BACK_OFF_DEGREE 4
+#define TAS_BACK_OFF_DEGREE 4
 
 /* Set the maximum sending attempts (default 20) */
-#define TS_MAX_ATTEMPTS   20
+#define TAS_MAX_ATTEMPTS   20
 
 #include <PJON.h>
 
-PJON<ThroughSerial> bus;
+PJON<ThroughAsyncSerial> bus;
 
 void setup() {
   Serial.begin(9600);
   bus.strategy.set_serial(&Serial);
+  bus.strategy.set_timeout(0); 
 }
 ```
 For a simple use with RS485 serial modules a transmission enable pin setter has been added:
@@ -60,4 +62,5 @@ HC-12 wireless module supports both synchronous and asynchronous acknowledgement
 All the other necessary information is present in the general [Documentation](/documentation).
 
 #### Known issues
-- Being PJON not an interrupt driven, its communication can be affected and potentially disrupted by long delays added in the user's sketch. Try to reduce as possible the interval between every `receive` call.
+- Send not ASYNC.
+- ACK not ASYNC. 
--- src/strategies/ThroughAysncSerial/ThroughAysncSerial.h
+++ src/strategies/ThroughAysncSerial/ThroughAysncSerial.h
@@ -43,105 +43,105 @@
 
 #include "Timing.h"
 
-//#define DEBUG_ Serial
-
-#if defined(DEBUG_)
-#define DEBUGF(_1, ...) { DEBUG_.printf_P( PSTR( "[%s, line %i] " _1), __func__, __LINE__ ,##__VA_ARGS__); }
-#pragma message("DEBUG enabled for ThroughAsyncSerial")
-#else
-#define DEBUGF(...) {} 
-#endif
-
 enum TAS_state_t : uint8_t {
   TAS_WAITING,
   TAS_RECEIVING,
-  TAS_WAITING_ESCAPE, 
+  TAS_WAITING_ESCAPE,
   TAS_WAITING_END,
   TAS_DONE
-}; 
+};
 
-class ThroughAsyncSerial {
-  public:
-    uint8_t buffer[PJON_PACKET_MAX_LENGTH]{0}; 
-    uint16_t position{0}; 
-    TAS_state_t state{TAS_WAITING}; 
-    #if defined(ARDUINO)
-      Stream *serial = NULL;
-    #elif defined(RPI)
-      int16_t serial = 0;
-    #elif defined(_WIN32)
-      Serial *serial = NULL;
-    #endif
-    /* Returns suggested delay related to the attempts passed as parameter: */
+class ThroughAsyncSerial
+{
+public:
+  uint8_t buffer[PJON_PACKET_MAX_LENGTH] {0};
+  uint16_t position{0};
+  TAS_state_t state{TAS_WAITING};
+#if defined(ARDUINO)
+  Stream *serial = NULL;
+#elif defined(RPI)
+  int16_t serial = 0;
+#elif defined(_WIN32)
+  Serial *serial = NULL;
+#endif
+  /* Returns suggested delay related to the attempts passed as parameter: */
 
-    uint32_t back_off(uint8_t attempts) {
-      uint32_t result = attempts;
-      for(uint8_t d = 0; d < TAS_BACK_OFF_DEGREE; d++)
-        result *= (uint32_t)(attempts);
-      return result;
-    };
+  uint32_t back_off(uint8_t attempts)
+  {
+    uint32_t result = attempts;
+    for (uint8_t d = 0; d < TAS_BACK_OFF_DEGREE; d++) {
+      result *= (uint32_t)(attempts);
+    }
+    return result;
+  };
 
-    /* Begin method, to be called before transmission or reception:
-       (returns always true) */
+  /* Begin method, to be called before transmission or reception:
+     (returns always true) */
 
-    bool begin(uint8_t additional_randomness = 0) {
-      PJON_DELAY_MICROSECONDS(
-        PJON_RANDOM(TAS_INITIAL_DELAY) + additional_randomness
-      );
-      //_last_byte = receive_byte();
-      return true;
-    };
-
-
-    /* Check if the channel is free for transmission: */
-
-    bool can_start() {
-      PJON_DELAY_MICROSECONDS(PJON_RANDOM(TAS_COLLISION_DELAY));
-      if(PJON_SERIAL_AVAILABLE(serial)) return false;
-      if((uint32_t)(PJON_MICROS() - _last_reception_time) < TAS_TIME_IN)
-        return false;
-      return true;
-    };
+  bool begin(uint8_t additional_randomness = 0)
+  {
+    PJON_DELAY_MICROSECONDS(
+      PJON_RANDOM(TAS_INITIAL_DELAY) + additional_randomness
+    );
+    return true;
+  };
 
 
-    /* Returns the maximum number of attempts for each transmission: */
+  /* Check if the channel is free for transmission: This might be changable... https://www.arduino.cc/en/Serial/AvailableForWrite*/
 
-    static uint8_t get_max_attempts() {
-      return TAS_MAX_ATTEMPTS;
-    };
+  bool can_start()
+  {
+    PJON_DELAY_MICROSECONDS(PJON_RANDOM(TAS_COLLISION_DELAY));
+    if (PJON_SERIAL_AVAILABLE(serial)) { return false; }
+    if ((uint32_t)(PJON_MICROS() - _last_reception_time) < TAS_TIME_IN) {
+      return false;
+    }
+    return true;
+  };
 
 
-    /* Handle a collision: */
+  /* Returns the maximum number of attempts for each transmission: */
 
-    void handle_collision() {
-      PJON_DELAY_MICROSECONDS(PJON_RANDOM(TAS_COLLISION_DELAY));
-    };
+  static uint8_t get_max_attempts()
+  {
+    return TAS_MAX_ATTEMPTS;
+  };
 
 
-    /* Receive byte response */
+  /* Handle a collision: */
 
-    uint16_t receive_response() {
-      uint32_t time = PJON_MICROS();
-      while((uint32_t)(PJON_MICROS() - time) < TAS_RESPONSE_TIME_OUT) {
-        if(PJON_SERIAL_AVAILABLE(serial)) {
-          _last_reception_time = PJON_MICROS();
-          int16_t read = PJON_SERIAL_READ(serial);
-          if(read >= 0) {
-            _last_byte = (uint8_t)read;
-            return _last_byte;
-          }
+  void handle_collision()
+  {
+    PJON_DELAY_MICROSECONDS(PJON_RANDOM(TAS_COLLISION_DELAY));
+  };
+
+
+  /* Receive byte response */
+
+  uint16_t receive_response()
+  {
+    uint32_t time = PJON_MICROS();
+    while ((uint32_t)(PJON_MICROS() - time) < TAS_RESPONSE_TIME_OUT) {
+      if (PJON_SERIAL_AVAILABLE(serial)) {
+        _last_reception_time = PJON_MICROS();
+        int16_t read = PJON_SERIAL_READ(serial);
+        if (read >= 0) {
+          _last_byte = (uint8_t)read;
+          return _last_byte;
         }
-        #if defined(_WIN32)
-          PJON_DELAY_MICROSECONDS(TAS_RESPONSE_TIME_OUT / 10);
-        #endif
       }
-      return TAS_FAIL;
+#if defined(_WIN32)
+      PJON_DELAY_MICROSECONDS(TAS_RESPONSE_TIME_OUT / 10);
+#endif
+    }
+    return TAS_FAIL;
 
-    };
+  };
 
-    /* Receive a string: */
+  /* Receive a string: */
 
-    uint16_t receive_string(uint8_t *string, uint16_t max_length) {
+  uint16_t receive_string(uint8_t *string, uint16_t max_length)
+  {
 
     if (_lastCallTime && PJON_MICROS() - _lastCallTime < _timeoutus) {
       return TAS_FAIL;
@@ -150,278 +150,254 @@ class ThroughAsyncSerial {
     _lastCallTime = PJON_MICROS(); //  allows time for uart to receive data...
 
     if (_last_reception_time && ( PJON_MICROS() - _last_reception_time > 1000000)) {
-        DEBUGF("Timeout _last_reception_time = %u\n", _last_reception_time);
-        _last_reception_time = 0; 
-        state = TAS_WAITING;           
+      _last_reception_time = 0;
+      state = TAS_WAITING;
     }
 
-      switch (state) {
-        case TAS_WAITING: {
-            _last_reception_time = 0; 
-            uint8_t value{TAS_NOT_ASSIGNED}; 
-            while(PJON_SERIAL_AVAILABLE(serial)) {
-              uint8_t value = PJON_SERIAL_READ(serial) ;  // see if it is a start frame... 
-              //DEBUGF("avail = %u, next = %c [%u]\n", PJON_SERIAL_AVAILABLE(serial), value, value);
-              if (value == TAS_START) {
-                //_last_reception_time = PJON_MICROS(); 
-                //DEBUGF("Timeout _last_reception_time set To %u\n", _last_reception_time);
-                state = TAS_RECEIVING; 
-                position = 0; 
-                return TAS_FAIL; 
-              }
-            }; 
-
-          break; 
-        }
-
-        case TAS_RECEIVING: {
-            uint16_t result;
-            //DEBUGF("Rec\n"); 
-
-
-
-            int count = 0; 
-            //DEBUGF("Available = %u\n", PJON_SERIAL_AVAILABLE(serial));
-
-            while(PJON_SERIAL_AVAILABLE(serial)) {
-              uint8_t value = PJON_SERIAL_READ(serial) ; 
-
-              if (value == TAS_FAIL) { state = TAS_WAITING; DEBUGF("TAS_FAIL\n");  return TAS_FAIL; }   //  byte consumed 
-               // Unescaped START byte stuffing violation
-              if(value == TAS_START) { state = TAS_WAITING; DEBUGF("START_FAIL\n"); return TAS_FAIL; }   //  byte consumed        
-              
-              if (value == TAS_ESC) {
-                DEBUGF("ESC Found: "); 
-                if (!PJON_SERIAL_AVAILABLE(serial)) {
-                  state = TAS_WAITING_ESCAPE; 
-                  DEBUGF("waiting for ESCP\n"); 
-                  return TAS_FAIL;                  
-                } else {
-
-                  value = PJON_SERIAL_READ(serial);
-                  value ^= TAS_ESC; 
-                   if((value != TAS_START) && (value != TAS_ESC) && (value != TAS_END)) {
-                     state = TAS_WAITING; //  real fail 
-                     DEBUGF("BAIL: target = %c (%2X), %u bytes written\n", value, value, count); 
-                     return TAS_FAIL; 
-                   }
-                  count++; 
-                  buffer[position++] = value;   
-                  DEBUGF("ESC written [%u] = %c (%2X)\n", position, value, value); 
-                  _last_reception_time = PJON_MICROS();    
-                  //DEBUGF("Timeout _last_reception_time set To %u\n", _last_reception_time);
-                  continue; 
-                }
-              }
-
-              if (max_length == 1) {
-                DEBUGF("MaxLen=1 goto END\n"); 
-                state = TAS_WAITING_END; 
-                return TAS_FAIL; 
-              }
-
-              if (position + 1 >= PJON_PACKET_MAX_LENGTH ) {
-                  DEBUGF("Over Len: Fail\n"); 
-                 state = TAS_WAITING; //  real fail 
-                 return TAS_FAIL;                 
-              }
-
-              if (value == TAS_END) {
-                state = TAS_DONE; 
-                return TAS_FAIL; 
-              }
-              //DEBUGF("BufferWritten [%u] = %c (%2X)\n", position, value, value); 
-              count++; 
-              buffer[position++] = value; 
-              _last_reception_time = PJON_MICROS(); 
-              //DEBUGF("Timeout _last_reception_time set To %u\n", _last_reception_time);
-
-
-            }
-
-            DEBUGF("%u bytes written\n", count); 
-            return TAS_FAIL; 
-          
-          break;
-        }
-
-        case TAS_WAITING_ESCAPE: 
-        {
-          DEBUGF("esp\n"); 
-         if( PJON_SERIAL_AVAILABLE(serial) ) {
-          uint8_t value = PJON_SERIAL_READ(serial); 
-          value ^= TAS_ESC; 
-          buffer[position++] = value; 
-          state = TAS_RECEIVING; 
-          return TAS_FAIL;
-         }
-          break; 
-        }
-
-        case TAS_WAITING_END: {
-           DEBUGF("end\n"); 
-          if( PJON_SERIAL_AVAILABLE(serial) ) {
-            uint8_t value = PJON_SERIAL_READ(serial); 
-            if (value == TAS_END) {
-              state = TAS_DONE; 
-              return TAS_FAIL; 
-            } else {
-              state = TAS_WAITING; // real fail; 
-              return TAS_FAIL; 
-            }
-          }
-          break; 
-        }
-
-        case TAS_DONE: {
-          
-          memcpy(&string[0], &buffer[0], position); 
-          uint16_t len = position;
+    switch (state) {
+    case TAS_WAITING: {
+      _last_reception_time = 0;
+      uint8_t value{TAS_NOT_ASSIGNED};
+      while (PJON_SERIAL_AVAILABLE(serial)) {
+        uint8_t value = PJON_SERIAL_READ(serial) ;  // see if it is a start frame...
+        if (value == TAS_START) {
+          state = TAS_RECEIVING;
           position = 0;
-          state = TAS_WAITING; 
-          _last_reception_time = 0;
+          return TAS_FAIL;
+        }
+      };
 
-          return len; 
+      break;
+    }
+
+    case TAS_RECEIVING: {
+      uint16_t result;
+
+      while (PJON_SERIAL_AVAILABLE(serial)) {
+        uint8_t value = PJON_SERIAL_READ(serial) ;
+
+        if (value == TAS_FAIL) { state = TAS_WAITING; return TAS_FAIL; }   //  byte consumed
+        // Unescaped START byte stuffing violation
+        if (value == TAS_START) { state = TAS_WAITING; return TAS_FAIL; }  //  byte consumed
+
+        if (value == TAS_ESC) {
+          if (!PJON_SERIAL_AVAILABLE(serial)) {
+            state = TAS_WAITING_ESCAPE;
+            return TAS_FAIL;
+          } else {
+            value = PJON_SERIAL_READ(serial);
+            value ^= TAS_ESC;
+            if ((value != TAS_START) && (value != TAS_ESC) && (value != TAS_END)) {
+              state = TAS_WAITING; //  real fail
+              return TAS_FAIL;
+            }
+            count++;
+            buffer[position++] = value;
+            _last_reception_time = PJON_MICROS();
+            continue;
+          }
         }
 
-      }; 
+        if (max_length == 1) {
+          state = TAS_WAITING_END;
+          return TAS_FAIL;
+        }
 
+        if (position + 1 >= PJON_PACKET_MAX_LENGTH ) {
+          state = TAS_WAITING; //  real fail
+          return TAS_FAIL;
+        }
+
+        if (value == TAS_END) {
+          state = TAS_DONE;
+          return TAS_FAIL;
+        }
+        buffer[position++] = value;
+        _last_reception_time = PJON_MICROS();
+      }
       return TAS_FAIL;
 
-    };
-
-    /*  sets the timeout for attempting to read data from serial to allow buffer to fill.  
-        different from back_off that is to do with send.   */
-    uint32_t get_timeout() { return _timeoutus; }
-    void set_timeout(uint32_t t) { _timeoutus = t ; }
-
-
-    /* Send a byte and wait for its transmission end */
-
-    void send_byte(uint8_t b) {
-      PJON_SERIAL_WRITE(serial, b);
-    };
-
-
-    /* Send byte response to the packet's transmitter */
-
-    void send_response(uint8_t response) {
-      start_tx();
-      send_byte(response);
-      PJON_SERIAL_FLUSH(serial);
-      end_tx();
-    };
-
-
-    /* Send a string: */
-
-    void send_string(uint8_t *string, uint8_t length) {
-      start_tx();
-      uint16_t overhead = 2;
-      // Add frame flag
-      send_byte(TAS_START);
-      for(uint8_t b = 0; b < length; b++) {
-        // Byte-stuffing
-        if(
-          (string[b] == TAS_START) ||
-          (string[b] == TAS_ESC) ||
-          (string[b] == TAS_END)
-        ) {
-          send_byte(TAS_ESC);
-          send_byte(string[b] ^ TAS_ESC);
-          overhead++;
-        } else send_byte(string[b]);
-      }
-      send_byte(TAS_END);
-      /* On RPI flush fails to wait until all bytes are transmitted
-         here RPI forced to wait blocking using delayMicroseconds */
-      #if defined(RPI)
-        if(_bd)
-          PJON_DELAY_MICROSECONDS(
-            ((1000000 / (_bd / 8)) + _flush_offset) * (overhead + length)
-          );
-      #endif
-      PJON_SERIAL_FLUSH(serial);
-      end_tx();
-      //Serial.println(); 
-    };
-
-
-    /* Pass the Serial port where you want to operate with */
-
-  #if defined(ARDUINO)
-    void set_serial(Stream *serial_port) {
-  #elif defined(RPI)
-    void set_serial(int16_t serial_port) {
-  #elif defined(_WIN32)
-    void set_serial(Serial *serial_port) {
-  #endif
-      serial = serial_port;
-    };
-
-
-    /* RS485 enable pins handling: */
-
-    void start_tx() {
-      if(_enable_RS485_txe_pin != TAS_NOT_ASSIGNED) {
-        PJON_IO_WRITE(_enable_RS485_txe_pin, HIGH);
-        if(_enable_RS485_rxe_pin != TAS_NOT_ASSIGNED)
-          PJON_IO_WRITE(_enable_RS485_rxe_pin, HIGH);
-      }
-    };
-
-    void end_tx() {
-      if(_enable_RS485_txe_pin != TAS_NOT_ASSIGNED) {
-        PJON_IO_WRITE(_enable_RS485_txe_pin, LOW);
-        if(_enable_RS485_rxe_pin != TAS_NOT_ASSIGNED)
-          PJON_IO_WRITE(_enable_RS485_rxe_pin, LOW);
-      }
-    };
-
-  #if defined(RPI)
-    /* Pass baudrate to ThroughSerial
-       (needed only for RPI flush hack): */
-
-    void set_baud_rate(uint32_t baud) {
-      _bd = baud;
-    };
-
-
-    /* Set flush timing offset in microseconds between expected and real
-       serial byte transmission: */
-
-    void set_flush_offset(uint16_t offset) {
-      _flush_offset = offset;
-    };
-  #endif
-
-    /* RS485 enable pins setters: */
-
-    void set_enable_RS485_pin(uint8_t pin) {
-      set_RS485_txe_pin(pin);
-    };
-
-    void set_RS485_rxe_pin(uint8_t pin) {
-      _enable_RS485_rxe_pin = pin;
-      PJON_IO_MODE(_enable_RS485_rxe_pin, OUTPUT);
+      break;
     }
 
-    void set_RS485_txe_pin(uint8_t pin) {
-      _enable_RS485_txe_pin = pin;
-      PJON_IO_MODE(_enable_RS485_txe_pin, OUTPUT);
+    case TAS_WAITING_ESCAPE: {
+      if ( PJON_SERIAL_AVAILABLE(serial) ) {
+        uint8_t value = PJON_SERIAL_READ(serial);
+        value ^= TAS_ESC;
+        buffer[position++] = value;
+        state = TAS_RECEIVING;
+        return TAS_FAIL;
+      }
+      break;
     }
 
-  private:
-  #if defined(RPI)
-    uint16_t _flush_offset = TAS_FLUSH_OFFSET;
-    uint32_t _bd;
-  #endif
-    uint8_t  _last_byte;
-    uint32_t _last_reception_time = 0;
-    uint8_t  _enable_RS485_rxe_pin = TAS_NOT_ASSIGNED;
-    uint8_t  _enable_RS485_txe_pin = TAS_NOT_ASSIGNED;
-    uint32_t _timeoutus = 100; 
-    uint32_t _lastCallTime = 0; 
+    case TAS_WAITING_END: {
+      if ( PJON_SERIAL_AVAILABLE(serial) ) {
+        uint8_t value = PJON_SERIAL_READ(serial);
+        if (value == TAS_END) {
+          state = TAS_DONE;
+          return TAS_FAIL;
+        } else {
+          state = TAS_WAITING; // real fail;
+          return TAS_FAIL;
+        }
+      }
+      break;
+    }
+
+    case TAS_DONE: {
+      memcpy(&string[0], &buffer[0], position);
+      uint16_t len = position;
+      position = 0;
+      state = TAS_WAITING;
+      _last_reception_time = 0;
+      return len;
+    }
+
+    };
+
+    return TAS_FAIL;
+
+  };
+
+  /*  sets the timeout for attempting to read data from serial to allow buffer to fill.
+      different from back_off that is to do with send.   */
+  uint32_t get_timeout() { return _timeoutus; }
+  void set_timeout(uint32_t t) { _timeoutus = t ; }
+
+
+  /* Send a byte and wait for its transmission end */
+
+  void send_byte(uint8_t b)
+  {
+    PJON_SERIAL_WRITE(serial, b);
+  };
+
+
+  /* Send byte response to the packet's transmitter */
+
+  void send_response(uint8_t response)
+  {
+    start_tx();
+    send_byte(response);
+    PJON_SERIAL_FLUSH(serial);
+    end_tx();
+  };
+
+
+  /* Send a string: */
+
+  void send_string(uint8_t *string, uint8_t length)
+  {
+    start_tx();
+    uint16_t overhead = 2;
+    // Add frame flag
+    send_byte(TAS_START);
+    for (uint8_t b = 0; b < length; b++) {
+      // Byte-stuffing
+      if (
+        (string[b] == TAS_START) ||
+        (string[b] == TAS_ESC) ||
+        (string[b] == TAS_END)
+      ) {
+        send_byte(TAS_ESC);
+        send_byte(string[b] ^ TAS_ESC);
+        overhead++;
+      } else { send_byte(string[b]); }
+    }
+    send_byte(TAS_END);
+    /* On RPI flush fails to wait until all bytes are transmitted
+       here RPI forced to wait blocking using delayMicroseconds */
+#if defined(RPI)
+    if (_bd)
+      PJON_DELAY_MICROSECONDS(
+        ((1000000 / (_bd / 8)) + _flush_offset) * (overhead + length)
+      );
+#endif
+    PJON_SERIAL_FLUSH(serial);
+    end_tx();
+    //Serial.println();
+  };
+
+
+  /* Pass the Serial port where you want to operate with */
+
+#if defined(ARDUINO)
+  void set_serial(Stream *serial_port)
+  {
+#elif defined(RPI)
+  void set_serial(int16_t serial_port) {
+#elif defined(_WIN32)
+  void set_serial(Serial * serial_port) {
+#endif
+    serial = serial_port;
+  };
+
+
+  /* RS485 enable pins handling: */
+
+  void start_tx() {
+    if (_enable_RS485_txe_pin != TAS_NOT_ASSIGNED) {
+      PJON_IO_WRITE(_enable_RS485_txe_pin, HIGH);
+      if (_enable_RS485_rxe_pin != TAS_NOT_ASSIGNED) {
+        PJON_IO_WRITE(_enable_RS485_rxe_pin, HIGH);
+      }
+    }
+  };
+
+  void end_tx() {
+    if (_enable_RS485_txe_pin != TAS_NOT_ASSIGNED) {
+      PJON_IO_WRITE(_enable_RS485_txe_pin, LOW);
+      if (_enable_RS485_rxe_pin != TAS_NOT_ASSIGNED) {
+        PJON_IO_WRITE(_enable_RS485_rxe_pin, LOW);
+      }
+    }
+  };
+
+#if defined(RPI)
+  /* Pass baudrate to ThroughSerial
+     (needed only for RPI flush hack): */
+
+  void set_baud_rate(uint32_t baud) {
+    _bd = baud;
+  };
+
+
+  /* Set flush timing offset in microseconds between expected and real
+     serial byte transmission: */
+
+  void set_flush_offset(uint16_t offset) {
+    _flush_offset = offset;
+  };
+#endif
+
+  /* RS485 enable pins setters: */
+
+  void set_enable_RS485_pin(uint8_t pin) {
+    set_RS485_txe_pin(pin);
+  };
+
+  void set_RS485_rxe_pin(uint8_t pin) {
+    _enable_RS485_rxe_pin = pin;
+    PJON_IO_MODE(_enable_RS485_rxe_pin, OUTPUT);
+  }
+
+  void set_RS485_txe_pin(uint8_t pin) {
+    _enable_RS485_txe_pin = pin;
+    PJON_IO_MODE(_enable_RS485_txe_pin, OUTPUT);
+  }
+
+private:
+#if defined(RPI)
+  uint16_t _flush_offset = TAS_FLUSH_OFFSET;
+  uint32_t _bd;
+#endif
+  uint8_t  _last_byte;
+  uint32_t _last_reception_time = 0;
+  uint8_t  _enable_RS485_rxe_pin = TAS_NOT_ASSIGNED;
+  uint8_t  _enable_RS485_txe_pin = TAS_NOT_ASSIGNED;
+  uint32_t _timeoutus = 100;
+  uint32_t _lastCallTime = 0;
 
 
 

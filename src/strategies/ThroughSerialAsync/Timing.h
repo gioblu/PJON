8b06cb73bf7055393cb25998ac944e50151294e7
unknown <gioscarab@gmail.com>
2018-08-29 22:31:37 +0200

    ThroughAysnSerial -> ThroughSerialAsync, codestyle, docs, minor fix

--- src/strategies/ThroughSerialAsync/Timing.h
+++ src/strategies/ThroughSerialAsync/Timing.h
@@ -8,7 +8,8 @@
     - Franketto (Arduino forum user) RS485 TX enable pin compatibility
    ____________________________________________________________________________
 
-   ThroughSerial, Copyright (c) 2016 by Giovanni Blu Mitolo All rights reserved.
+   ThroughSerialAsync timing, based on ThroughSerial, developed by sticilface
+   copyright 2018 by Giovanni Blu Mitolo All rights reserved
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -25,42 +26,53 @@
 #pragma once
 
 /* Maximum 1 second random initial delay */
-#ifndef TAS_INITIAL_DELAY
-  #define TAS_INITIAL_DELAY      1000
+#ifndef TSA_INITIAL_DELAY
+  #define TSA_INITIAL_DELAY      1000
 #endif
 
 /* Mamimum 32 microseconds random delay in case of collision */
-#ifndef TAS_COLLISION_DELAY
-  #define TAS_COLLISION_DELAY      32
+#ifndef TSA_COLLISION_DELAY
+  #define TSA_COLLISION_DELAY      32
 #endif
 
 /* Set 10 milliseconds as the maximum timeframe between transmission and
-   synchronous acknowledgement response. This value is strictly related to the
-   maximum time needed by receiver to receive, compute and transmit a response.
-   Higher if necessary. */
-#ifndef TAS_RESPONSE_TIME_OUT
-  #define TAS_RESPONSE_TIME_OUT 10000
+   synchronous acknowledgement response. Its  optimal configuration is
+   strictly related to the maximum time needed by receiver to receive, compute
+   and transmit back an ACK (decimal 6) */
+#ifndef TSA_RESPONSE_TIME_OUT
+  #define TSA_RESPONSE_TIME_OUT 10000
 #endif
 
 /* Minum timeframe with channel free for use before transmission.
    (Avoid disrupting synchronous acknowledgement exchange) */
-#ifndef TAS_TIME_IN
-  #define TAS_TIME_IN  TAS_RESPONSE_TIME_OUT + TAS_COLLISION_DELAY
+#ifndef TSA_TIME_IN
+  #define TSA_TIME_IN  TSA_RESPONSE_TIME_OUT + TSA_COLLISION_DELAY
 #endif
 
-/* Set 5 milliseconds as the maximum timeframe for byte reception.
-   This value depends on the latency, baud rate and computation time.
-   Always set TS_BYTE_TIME_OUT > (byte transmission time + latency) */
-#ifndef TAS_BYTE_TIME_OUT
-  #define TAS_BYTE_TIME_OUT      5000
+/* Set 100 microseconds as the interval between each byte read.
+   Depending on the latency, baud rate and computation time the
+   optimal TSA_READ_INTERVAL value may variate.
+   Always set: TSA_READ_INTERVAL > (byte transmission time + latency) */
+#ifndef TSA_READ_INTERVAL
+  #define TSA_READ_INTERVAL       100
+#endif
+
+/* Byte reception timeout */
+#ifndef TSA_BYTE_TIME_OUT
+  #define TSA_BYTE_TIME_OUT   1000000
 #endif
 
 /* Maximum transmission attempts */
-#ifndef TAS_MAX_ATTEMPTS
-  #define TAS_MAX_ATTEMPTS         20
+#ifndef TSA_MAX_ATTEMPTS
+  #define TSA_MAX_ATTEMPTS         20
 #endif
 
 /* Back-off exponential degree */
-#ifndef TAS_BACK_OFF_DEGREE
-  #define TAS_BACK_OFF_DEGREE      4
+#ifndef TSA_BACK_OFF_DEGREE
+  #define TSA_BACK_OFF_DEGREE       4
+#endif
+
+/* Delay before enabling and disabling RS485 DE and or RE pin */
+#ifndef TSA_RS485_DELAY
+  #define TSA_RS485_DELAY           1
 #endif

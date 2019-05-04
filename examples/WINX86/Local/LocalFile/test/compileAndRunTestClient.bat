7e86a99aa1b43540a814f913fd118c6f16c2383d
unknown <gioscarab@gmail.com>
2019-05-04 17:44:22 +0200

    LocalFile outdated examples removed

--- examples/WINX86/Local/LocalFile/test/compileAndRunTestClient.bat
+++
@@ -1,3 +0,0 @@
-cls
-g++ -std=c++11 -I..\..\..  TestClient.cpp -o TestClient
-TestClient %1 %2 %3
--- examples/WINX86/Local/LocalFile/test/stresstest.bat
+++
@@ -1,9 +0,0 @@
-%ECHO OFF
-cls
-for /L %%N in (2,1,%2) do (
-    start TestClient %%N %1 %3
-)
-timeout 2
-TestSender %1 %2
-echo Total errors: %errorlevel%
-

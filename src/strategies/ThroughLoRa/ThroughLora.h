03589c4fc2a1669c8e0e946fb1a9869cf5c79391
unknown <gioscarab@gmail.com>
2019-08-11 02:10:05 +0200

    ThroughLoRa tabs fix

--- src/strategies/ThroughLoRa/ThroughLora.h
+++ src/strategies/ThroughLoRa/ThroughLora.h
@@ -55,46 +55,46 @@ class ThroughLora {
       LoRa.setPins(cs_pin, reset_pin, dio0_pin);
     };
 
-		void setSpreadingFactor(uint8_t spreadingFactor) {
-		  LoRa.setSpreadingFactor(spreadingFactor);
-		};
+    void setSpreadingFactor(uint8_t spreadingFactor) {
+      LoRa.setSpreadingFactor(spreadingFactor);
+    };
 
-		void setCodingRate4(uint8_t codingRate) {
-		  LoRa.setCodingRate4(codingRate);
-		};
+    void setCodingRate4(uint8_t codingRate) {
+      LoRa.setCodingRate4(codingRate);
+    };
 
-		void setPreambleLength(uint16_t preambleLength) {
-			LoRa.setPreambleLength(preambleLength);
-		};
+    void setPreambleLength(uint16_t preambleLength) {
+      LoRa.setPreambleLength(preambleLength);
+    };
 
-		void setSyncWord(uint8_t syncWord) {
-			LoRa.setSyncWord(syncWord);
-		};
+    void setSyncWord(uint8_t syncWord) {
+      LoRa.setSyncWord(syncWord);
+    };
 
-		void setCRC(bool enableCRC) {
-			if (enableCRC) LoRa.enableCrc();
-			else LoRa.disableCrc();
-		};
+    void setCRC(bool enableCRC) {
+      if (enableCRC) LoRa.enableCrc();
+      else LoRa.disableCrc();
+    };
 
-		void setTxPower(uint8_t txPower) {
-			LoRa.setTxPower(txPower);
-		};
+    void setTxPower(uint8_t txPower) {
+      LoRa.setTxPower(txPower);
+    };
 
-		void setTxPower(uint8_t txPower, uint8_t boostPin) {
-			LoRa.setTxPower(txPower, boostPin);
-		};
+    void setTxPower(uint8_t txPower, uint8_t boostPin) {
+      LoRa.setTxPower(txPower, boostPin);
+    };
 
-		void idle(uint8_t txPower, uint8_t boostPin) {
-			LoRa.idle();
-		};
+    void idle(uint8_t txPower, uint8_t boostPin) {
+      LoRa.idle();
+    };
 
-		void sleep(uint8_t txPower, uint8_t boostPin) {
-			LoRa.sleep();
-		};
+    void sleep(uint8_t txPower, uint8_t boostPin) {
+      LoRa.sleep();
+    };
 
-		uint8_t getRandom() {
-			return LoRa.random();
-		};
+    uint8_t getRandom() {
+      return LoRa.random();
+    };
 
     /* Begin method, to be called on initialization:
        (returns always true) */

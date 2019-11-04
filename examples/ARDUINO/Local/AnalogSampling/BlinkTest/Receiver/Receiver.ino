003053801ab1b5db402c75567ac3533fc3fb14fe
unknown <gioscarab@gmail.com>
2019-11-04 10:24:17 +0100

    AnalogSampling examples comments fix thanks to @UladzimirKapylou

--- examples/ARDUINO/Local/AnalogSampling/BlinkTest/Receiver/Receiver.ino
+++ examples/ARDUINO/Local/AnalogSampling/BlinkTest/Receiver/Receiver.ino
@@ -1,11 +1,8 @@
 #include <PJON.h>
 
 /* Use a couple of visible light / IR / UV LEDs as wireless bidirectional transceivers
-   Connect LED + to A0
-   Connect LED - to GND
-   Place a 1-5 MΩ pull down resistor between A0 and GND
-   Try different resistor values to find the optimal to maximize range
-   Higher resistance can higher the range but can also higher background noise.  */
+   To know how to wire up the circuit see the AnalogSampling README:
+   https://github.com/gioblu/PJON/tree/master/src/strategies/AnalogSampling */
 
 // <Strategy name> bus(selected device id)
 PJON<AnalogSampling> bus(44);

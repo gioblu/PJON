de6bff8104f06bc163c1182b33de4948c5c4faba
Stephan Alsleben <stephan.alsleben@atsonline.de>
2020-09-01 16:12:47 +0200

    zephyr: add a cpp file for PJON_ZEPHYR_Interface

--- src/interfaces/ZEPHYR/PJON_ZEPHYR_Interface.h
+++ src/interfaces/ZEPHYR/PJON_ZEPHYR_Interface.h
@@ -1,73 +1,23 @@
+#pragma once
+
 #if defined(ZEPHYR)
 
 #define OUTPUT GPIO_OUTPUT
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <drivers/gpio.h>
-#include <drivers/uart.h>
-#include <sys/ring_buffer.h>
-#include <logging/log.h>
-#include <devicetree.h>
+#include <zephyr.h>
+#include <stdint.h>
+#include <string>
 
-RING_BUF_DECLARE(ringbuffer_pjon, CONFIG_PJON_MTU);
-static uint8_t tmp_buffer[40];
-static int serial_get_char(struct device* dev)
-{
-    size_t  ret;
-    uint8_t b;
-    ret = ring_buf_get(&ringbuffer_pjon, &b, sizeof(b));
-    return b;
-}
-
-static bool serial_char_available(struct device* dev)
-{
-    bool b = ring_buf_is_empty(&ringbuffer_pjon);
-    return !b;
-}
-
-static void uart_irq_callback(struct device *dev)
-{
-	uart_irq_update(dev);
-
-	if (uart_irq_rx_ready(dev)) {
-        uint8_t tmp;
-		while (1) {
-			if (uart_fifo_read(dev, &tmp, 1) == 0) {
-				break;
-			}
-            ring_buf_put(&ringbuffer_pjon, &tmp, sizeof(tmp));
-		}
-    }
-}
-
-static struct device* pin_dev = NULL;
-
-static int serial_put_char(struct device* dev, uint8_t byte)
-{
-    uart_poll_out(dev,byte);
-    return 1;
-}
-
-static void digitalWrite(int pin, bool state)
-{
-    gpio_pin_set(pin_dev, pin, state);
-
-}
-static void pinMode(int pin, int mode)
-{
-    pin_dev = device_get_binding(DT_GPIO_LABEL(DT_ALIAS(pjon_txe_pin), gpios));
-
-    gpio_pin_configure(pin_dev, pin, mode);
-
-}
-
-static void serial_flush(struct device* dev)
-{
-    k_usleep(10);
-}
+int serial_get_char(struct device* dev);
+bool serial_char_available(struct device* dev);
+void uart_irq_callback(struct device *dev);
+int serial_put_char(struct device* dev, uint8_t byte);
+struct device* serial_open(const char* dt_label);
+void serial_close(const char* dt_label);
+void digitalWrite(int pin, bool state);
+void pinMode(int pin, int mode);
+void serial_flush(struct device* dev);
 
 // deal with randomness
 
